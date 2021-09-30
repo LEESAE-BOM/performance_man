736df25463894fdf57cf6695d29a6fbf3e9660bf
sseunnxx <89835693+sseunnxx@users.noreply.github.com>
2021-10-01 00:51:22 +0900

    Update sales.dart

--- Front/lib/management_widget/sales.dart
+++ Front/lib/management_widget/sales.dart
@@ -1,16 +1,208 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
+class sales extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _sales();
+}
 
-class sales extends StatelessWidget {
-  // This widget is the root of your application.
+class _sales extends State<sales> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-            child: Center(
-                child: Text('매출금액')
-            )
-        )
+      appBar: AppBar(
+        title: Text('매출 금액'),
+        centerTitle: true,
+        backgroundColor: Colors.indigo,
+      ),
+      body: SafeArea(
+        child: Padding(
+            padding: EdgeInsets.fromLTRB(40.0, 40.0, 40.0, 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text.rich(TextSpan(
+                    text: '이번 달 ',
+                    style: TextStyle(fontSize: 25.0, letterSpacing: 2.0),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: '매출 금액',
+                        style: TextStyle(
+                            fontSize: 30.0,
+                            color: Colors.blue,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 2.0),
+                      ),
+                      TextSpan(
+                          text: '은',
+                          style: TextStyle(fontSize: 25.0, letterSpacing: 2.0)),
+                    ])),
+                SizedBox(
+                  height: 15.0,
+                ),
+                Column(
+                  children: <Widget>[
+                    Text.rich(TextSpan(text: '', children: <TextSpan>[
+                      TextSpan(
+                        text: '123,456,789원 ',
+                        style: TextStyle(
+                          fontSize: 30.0,
+                          color: Colors.blue,
+                          letterSpacing: 2.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      TextSpan(
+                          text: '이에요.',
+                          style: TextStyle(
+                              fontSize: 25.0,
+                              letterSpacing: 2.0,
+                              color: Colors.black)),
+                    ]))
+                  ],
+                ),
+                SizedBox(
+                  height: 15.0,
+                ),
+                Column(
+                  children: <Widget>[
+                    Text.rich(TextSpan(
+                        text: '전월 대비 ',
+                        style: TextStyle(fontSize: 25.0, letterSpacing: 2.0),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: '1,000원 증가 ',
+                            style: TextStyle(
+                                fontSize: 30.0,
+                                color: Colors.blue,
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: 2.0),
+                          ),
+                          TextSpan(
+                              text: '했어요.',
+                              style: TextStyle(
+                                fontSize: 25.0,
+                                letterSpacing: 2.0,
+                              )),
+                        ]))
+                  ],
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                Center(
+                  child: Container(
+                    width: double.infinity,
+                    child: Theme(
+                      data:
+                          Theme.of(context).copyWith(dividerColor: Colors.grey),
+                      child: DataTable(
+                        showBottomBorder: true,
+                        headingRowColor: MaterialStateColor.resolveWith(
+                            (states) => Colors.black12),
+                        columns: <DataColumn>[
+                          DataColumn(
+                            label: Container(
+                              width: 100.0,
+                              child: Text(
+                                '',
+                              ),
+                            ),
+                          ),
+                          DataColumn(
+                            label: Container(
+                              width: 150.0,
+                              alignment: Alignment.center,
+                              child: Text('매출 금액 내역',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 23.0)),
+                            ),
+                          ),
+                          DataColumn(
+                            label: Container(
+                              width: 100.0,
+                              child: Text(
+                                '',
+                              ),
+                            ),
+                          ),
+                        ],
+                        rows: <DataRow>[
+                          DataRow(
+                            cells: <DataCell>[
+                              DataCell(Text('')),
+                              DataCell(Text('')),
+                              DataCell(Container(
+                                  width: 150.0,
+                                  child: Text(
+                                    '최근 1개월',
+                                    textAlign: TextAlign.right,
+                                    style: TextStyle(
+                                        fontSize: 15.0,
+                                        fontWeight: FontWeight.bold),
+                                  )))
+                            ],
+                          ),
+                          DataRow(
+                            cells: <DataCell>[
+                              DataCell(
+                                Container(
+                                  child: Text(
+                                    '월',
+                                    textAlign: TextAlign.right,
+                                    style: TextStyle(fontSize: 18.0),
+                                  ),
+                                  width: 100.0,
+                                ),
+                              ),
+                              DataCell(Text('')),
+                              DataCell(
+                                Container(
+                                    width: 150.0,
+                                    child: Text(
+                                      '45,600원',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(fontSize: 18.0),
+                                    )),
+                              ),
+                            ],
+                          ),
+                          DataRow(
+                            color: MaterialStateColor.resolveWith(
+                                    (states) => Colors.black12),
+                            cells: <DataCell>[
+                              DataCell(
+                                Container(
+                                  child: Text(
+                                    '10월',
+                                    textAlign: TextAlign.right,
+                                    style: TextStyle(fontSize: 18.0),
+                                  ),
+                                  width: 100.0,
+                                ),
+                              ),
+                              DataCell(Text('')),
+                              DataCell(
+                                Container(
+                                    width: 150.0,
+                                    child: Text(
+                                      '123,123,568원',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(fontSize: 18.0),
+                                    )),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            )),
+      ),
     );
   }
 }
