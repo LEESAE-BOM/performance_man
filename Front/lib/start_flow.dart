4efcac5b60e8dc578f711d97798949794fc5f748
sseunnxx <89835693+sseunnxx@users.noreply.github.com>
2021-10-01 02:44:10 +0900

    Update start_flow.dart

--- Front/lib/start_flow.dart
+++ Front/lib/start_flow.dart
@@ -1,14 +1,31 @@
+import 'dart:async';
+import 'main.dart';
 import 'package:flutter/material.dart';
 
-
-class energy_main extends StatelessWidget {
+class start_flow extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  _start_flowState createState() => _start_flowState();
+}
+
+class _start_flowState extends State<start_flow> {
+
+  Future<Timer> loadData() async{
+    return Timer(Duration(seconds: 5),onDoneLoading);
+  }
+
+  onDoneLoading() async{
+    Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>MyApp()));
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromRGBO(43, 63, 107,1),
         body: Container(
             child: Center(
-                child: Text('시작화면')
+                child: Image.asset('image/logo.png',
+                width: 250.0,height: 200.0,
+                ),
             )
         )
     );
