abddafe2473a6674501ea0019fd8cb26aea1f59b
sseunnxx <89835693+sseunnxx@users.noreply.github.com>
2021-10-02 01:14:21 +0900

    Update outsourcing_ratio.dart

--- Front/lib/management_widget/outsourcing_ratio.dart
+++ Front/lib/management_widget/outsourcing_ratio.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class outsourcing_ratio extends StatefulWidget {
   @override
@@ -9,68 +10,33 @@ class outsourcing_ratio extends StatefulWidget {
 class _outsourcing_ratio extends State<outsourcing_ratio> {
   @override
   Widget build(BuildContext context) {
+    final List<ChartData> chartData = [
+      ChartData('A사', 45),
+      ChartData('B사', 55),
+    ];
     return Scaffold(
       appBar: AppBar(
         title: Text('외주 비율'),
         centerTitle: true,
-        backgroundColor: Color.fromRGBO(43, 63, 107,1),
+        backgroundColor: Color.fromRGBO(43, 63, 107, 1),
       ),
       body: SafeArea(
         child: Padding(
             padding: EdgeInsets.fromLTRB(40.0, 40.0, 40.0, 0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+            child: ListView(
               children: <Widget>[
-                Text.rich(TextSpan(
-                    text: '전체 ',
-                    style: TextStyle(fontSize: 25.0, letterSpacing: 2.0),
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: '수주 비용 ',
-                        style: TextStyle(
-                            fontSize: 30.0,
-                            color: Colors.blue,
-                            fontWeight: FontWeight.bold,
-                            letterSpacing: 2.0),
-                      ),
-                      TextSpan(
-                          text: '중 ',
-                          style: TextStyle(fontSize: 25.0, letterSpacing: 2.0)),
-                    ])),
-                SizedBox(
-                  height: 15.0,
-                ),
                 Column(
-                  children: <Widget>[
-                    Text.rich(TextSpan(text: '', children: <TextSpan>[
-                      TextSpan(
-                        text: '외주 비율',
-                        style: TextStyle(
-                          fontSize: 30.0,
-                          color: Colors.blue,
-                          letterSpacing: 2.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      TextSpan(
-                          text: '이 차지하는 비율은 ',
-                          style: TextStyle(
-                              fontSize: 25.0,
-                              letterSpacing: 2.0,
-                              color: Colors.black)),
-                    ]))
-                  ],
-                ),
-                SizedBox(
-                  height: 15.0,
-                ),
-                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
                     Text.rich(TextSpan(
-                        text: '',
+                        text: '전체 ',
+                        style: TextStyle(
+                            fontSize: 25.0,
+                            letterSpacing: 2.0,
+                            fontFamily: 'AppleB'),
                         children: <TextSpan>[
                           TextSpan(
-                            text: '약 40% ',
+                            text: '수주 비용 ',
                             style: TextStyle(
                                 fontSize: 30.0,
                                 color: Colors.blue,
@@ -78,105 +44,189 @@ class _outsourcing_ratio extends State<outsourcing_ratio> {
                                 letterSpacing: 2.0),
                           ),
                           TextSpan(
+                              text: '중 ',
+                              style: TextStyle(
+                                  fontSize: 25.0, letterSpacing: 2.0)),
+                        ])),
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Text.rich(TextSpan(text: '', children: <TextSpan>[
+                          TextSpan(
+                            text: '외주 비율',
+                            style: TextStyle(
+                                fontSize: 30.0,
+                                color: Colors.blue,
+                                letterSpacing: 2.0,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'AppleB'),
+                          ),
+                          TextSpan(
+                              text: '이 차지하는 비율은 ',
+                              style: TextStyle(
+                                  fontSize: 25.0,
+                                  letterSpacing: 2.0,
+                                  color: Colors.black,
+                                  fontFamily: 'AppleB')),
+                        ]))
+                      ],
+                    ),
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Text.rich(TextSpan(text: '', children: <TextSpan>[
+                          TextSpan(
+                            text: '약 40% ',
+                            style: TextStyle(
+                                fontSize: 30.0,
+                                color: Colors.blue,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'AppleB',
+                                letterSpacing: 2.0),
+                          ),
+                          TextSpan(
                               text: '이에요.',
                               style: TextStyle(
-                                fontSize: 25.0,
-                                letterSpacing: 2.0,
-                              )),
+                                  fontSize: 25.0,
+                                  letterSpacing: 2.0,
+                                  fontFamily: 'AppleB')),
                         ]))
-                  ],
-                ),
-                SizedBox(
-                  height: 30.0,
-                ),
-                Center(
-                  child: Container(
-                    width: double.infinity,
-                    child: Theme(
-                      data:
-                      Theme.of(context).copyWith(dividerColor: Colors.grey),
-                      child: DataTable(
-                        showBottomBorder: true,
-                        headingRowColor: MaterialStateColor.resolveWith(
+                      ],
+                    ),
+                    SizedBox(
+                      height: 30.0,
+                    ),
+                    Center(
+                        child: Container(
+                            width: 500.0,
+                            height: 500.0,
+                            child: SfCircularChart(
+                                title: ChartTitle(
+                                    text: '2021',
+                                    textStyle: TextStyle(
+                                        fontSize: 35.0,
+                                        fontWeight: FontWeight.bold)),
+                                legend: Legend(
+                                    isVisible: true,
+                                    // Legend will be placed at the left
+                                    position: LegendPosition.bottom),
+                                series: <CircularSeries>[
+                                  // Render pie chart
+                                  PieSeries<ChartData, String>(
+                                      dataSource: chartData,
+                                      xValueMapper: (ChartData data, _) =>
+                                          data.x,
+                                      yValueMapper: (ChartData data, _) =>
+                                          data.y,
+                                      dataLabelSettings: DataLabelSettings(
+                                          isVisible: true,
+                                          // Positioning the data label
+                                          labelPosition:
+                                              ChartDataLabelPosition.outside)),
+                                ]))),
+                    SizedBox(
+                      height: 30.0,
+                    ),
+                    Center(
+                      child: Container(
+                        width: double.infinity,
+                        child: Theme(
+                          data: Theme.of(context)
+                              .copyWith(dividerColor: Colors.grey),
+                          child: DataTable(
+                            showBottomBorder: true,
+                            headingRowColor: MaterialStateColor.resolveWith(
                                 (states) => Colors.black12),
-                        columns: <DataColumn>[
-                          DataColumn(
-                            label: Container(
-                              width: 100.0,
-                              child: Text(
-                                '',
-                              ),
-                            ),
-                          ),
-                          DataColumn(
-                            label: Container(
-                              width: 150.0,
-                              alignment: Alignment.center,
-                              child: Text('외주 내역',
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 23.0)),
-                            ),
-                          ),
-                          DataColumn(
-                            label: Container(
-                              width: 100.0,
-                              child: Text(
-                                '',
-                              ),
-                            ),
-                          ),
-                        ],
-                        rows: <DataRow>[
-                          DataRow(
-                            cells: <DataCell>[
-                              DataCell(Text('')),
-                              DataCell(Text('')),
-                              DataCell(Container(
-                                  width: 150.0,
-                                  child: Text(
-                                    '최근 3개월',
-                                    textAlign: TextAlign.right,
-                                    style: TextStyle(
-                                        fontSize: 15.0,
-                                        fontWeight: FontWeight.bold),
-                                  )))
-                            ],
-                          ),
-                          DataRow(
-                            cells: <DataCell>[
-                              DataCell(
-                                Container(
-                                  child: Text(
-                                    '#,###,###',
-                                    textAlign: TextAlign.right,
-                                    style: TextStyle(fontSize: 18.0),
-                                  ),
+                            columns: <DataColumn>[
+                              DataColumn(
+                                label: Container(
                                   width: 100.0,
+                                  child: Text(
+                                    '',
+                                  ),
                                 ),
                               ),
-                              DataCell(
-                                Container(
-                                    width: 150.0,
-                                    child: Text(
-                                      '##사',
+                              DataColumn(
+                                label: Container(
+                                  width: 150.0,
+                                  alignment: Alignment.center,
+                                  child: Text('외주 내역',
                                       textAlign: TextAlign.center,
-                                      style: TextStyle(fontSize: 18.0),
-                                    )),
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 23.0)),
+                                ),
                               ),
-                              DataCell(Text('')
+                              DataColumn(
+                                label: Container(
+                                  width: 100.0,
+                                  child: Text(
+                                    '',
+                                  ),
+                                ),
+                              ),
+                            ],
+                            rows: <DataRow>[
+                              DataRow(
+                                cells: <DataCell>[
+                                  DataCell(Text('')),
+                                  DataCell(Text('')),
+                                  DataCell(Container(
+                                      width: 150.0,
+                                      child: Text(
+                                        '최근 3개월',
+                                        textAlign: TextAlign.right,
+                                        style: TextStyle(
+                                            fontSize: 15.0,
+                                            fontWeight: FontWeight.bold),
+                                      )))
+                                ],
+                              ),
+                              DataRow(
+                                cells: <DataCell>[
+                                  DataCell(
+                                    Container(
+                                      child: Text(
+                                        '#,###,###',
+                                        textAlign: TextAlign.right,
+                                        style: TextStyle(fontSize: 18.0),
+                                      ),
+                                      width: 100.0,
+                                    ),
+                                  ),
+                                  DataCell(
+                                    Container(
+                                        width: 150.0,
+                                        child: Text(
+                                          '##사',
+                                          textAlign: TextAlign.center,
+                                          style: TextStyle(fontSize: 18.0),
+                                        )),
+                                  ),
+                                  DataCell(Text('')),
+                                ],
                               ),
                             ],
                           ),
-                        ],
+                        ),
                       ),
-                    ),
-                  ),
-                )
+                    )
+                  ],
+                ),
               ],
             )),
       ),
     );
   }
+}
+
+class ChartData {
+  ChartData(this.x, this.y);
+
+  final String x;
+  final double y;
 }
