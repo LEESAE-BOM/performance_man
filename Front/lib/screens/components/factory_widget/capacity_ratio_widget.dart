6df92be04b4c40bb8b591d6bdb9c43d2b1d1527b
ssongdda <70890888+ssongdda@users.noreply.github.com>
2021-10-04 19:44:17 +0900

    공장 메인 위젯 화면 이동 구현

--- Front/lib/screens/components/factory_widget/capacity_ratio_widget.dart
+++ Front/lib/screens/components/factory_widget/capacity_ratio_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/factory_widget/capacity_ratio.dart';
 
 class Capacity_Ratio_Widget extends StatefulWidget {
   @override
@@ -8,47 +9,53 @@ class Capacity_Ratio_Widget extends StatefulWidget {
 class _Capacity_Ratio_Widget extends State<Capacity_Ratio_Widget> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        height: 180,
-        width: 180,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(20),
-          border: Border.all(color: Colors.black12, width: 3),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey,
-              offset: Offset(0.0, 1.0), //(x,y)
-              blurRadius: 6.0,
+    return GestureDetector(
+
+        onTap: () {
+          Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) => capacity_ratio()));
+        },
+        child: Container(
+            height: 180,
+            width: 180,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(color: Colors.black12, width: 3),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  offset: Offset(0.0, 1.0), //(x,y)
+                  blurRadius: 6.0,
+                ),
+              ],
             ),
-          ],
-        ),
-        //margin: EdgeInsets.all(30),
-        child: Column(
-            children: <Widget>[
-              Container(
-                height: 50,
-                padding: EdgeInsets.only(top: 5, bottom: 5, left: 10),
-                child: Row(
-                  children: [
-                    Text(
-                      '설비가동율',
-                      style: TextStyle(color: Colors.black54, fontSize: 15),
+            //margin: EdgeInsets.all(30),
+            child: Column(
+                children: <Widget>[
+                  Container(
+                    height: 50,
+                    padding: EdgeInsets.only(top: 5, bottom: 5, left: 10),
+                    child: Row(
+                      children: [
+                        Text(
+                          '설비가동율',
+                          style: TextStyle(color: Colors.black54, fontSize: 15),
+                        ),
+                        Image.asset(
+                          'image/danger.png',
+                          width: 10,
+                          height: 10,
+                        ),
+                      ],
                     ),
-                    Image.asset(
-                      'image/danger.png',
-                      width: 10,
-                      height: 10,
+                  ),
+                  Container(
+                    child: Text(
+                      '내용',
+                      style: TextStyle(color: Colors.black54, fontSize: 20),
                     ),
-                  ],
-                ),
-              ),
-              Container(
-                child: Text(
-                  '내용',
-                  style: TextStyle(color: Colors.black54, fontSize: 20),
-                ),
-              )
-            ]));
+                  )
+                ])));
   }
 }
