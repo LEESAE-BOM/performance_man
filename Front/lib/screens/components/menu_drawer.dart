fec0e1cdca89d33460c8dd744be5a40f95631a5a
ssongdda <70890888+ssongdda@users.noreply.github.com>
2021-10-04 15:42:27 +0900

    메뉴 서랍 추가

---
+++ Front/lib/screens/components/menu_drawer.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MenuDrawer extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Container(
+      width: 200,
+      height: double.infinity,
+      color: Colors.white,
+    );
+  }
+}
