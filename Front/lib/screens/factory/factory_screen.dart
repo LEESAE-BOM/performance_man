9b328d115ff62de92c4ec15a2cd82f7f34c791ec
ssongdda <70890888+ssongdda@users.noreply.github.com>
2021-10-04 15:46:02 +0900

    Create factory_screen.dart

---
+++ Front/lib/screens/factory/factory_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Factory_Screen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Center(
+      child: Text('factory_screen'),
+    );
+  }
+}
