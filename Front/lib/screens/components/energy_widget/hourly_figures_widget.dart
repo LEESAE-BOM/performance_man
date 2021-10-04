bd13c21b6c891004284680c98a32a32a41b81d9e
ssongdda <70890888+ssongdda@users.noreply.github.com>
2021-10-04 19:43:31 +0900

    에너지 메인 위젯 화면 이동 구현

--- Front/lib/screens/components/energy_widget/hourly_figures_widget.dart
+++ Front/lib/screens/components/energy_widget/hourly_figures_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/energy_widget/hourly_figures.dart';
 
 class Hourly_Figures_Widget extends StatefulWidget {
   @override
@@ -8,47 +9,53 @@ class Hourly_Figures_Widget extends StatefulWidget {
 class _Hourly_Figures_Widget extends State<Hourly_Figures_Widget> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        height: 180,
-        width: 180,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(20),
-          border: Border.all(color: Colors.black12, width: 3),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey,
-              offset: Offset(0.0, 1.0), //(x,y)
-              blurRadius: 6.0,
+    return GestureDetector(
+
+        onTap: () {
+          Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) => hourly_figures()));
+        },
+        child: Container(
+            height: 180,
+            width: 180,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(color: Colors.black12, width: 3),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  offset: Offset(0.0, 1.0), //(x,y)
+                  blurRadius: 6.0,
+                ),
+              ],
             ),
-          ],
-        ),
-        //margin: EdgeInsets.all(30),
-        child: Column(
-            children: <Widget>[
-              Container(
-                height: 50,
-                padding: EdgeInsets.only(top: 5, bottom: 5, left: 10),
-                child: Row(
-                  children: [
-                    Text(
-                      '피크예상시간',
-                      style: TextStyle(color: Colors.black54, fontSize: 15),
+            //margin: EdgeInsets.all(30),
+            child: Column(
+                children: <Widget>[
+                  Container(
+                    height: 50,
+                    padding: EdgeInsets.only(top: 5, bottom: 5, left: 10),
+                    child: Row(
+                      children: [
+                        Text(
+                          '피크예상시간',
+                          style: TextStyle(color: Colors.black54, fontSize: 15),
+                        ),
+                        Image.asset(
+                          'image/safe.png',
+                          width: 10,
+                          height: 10,
+                        ),
+                      ],
                     ),
-                    Image.asset(
-                      'image/safe.png',
-                      width: 10,
-                      height: 10,
+                  ),
+                  Container(
+                    child: Text(
+                      '내용',
+                      style: TextStyle(color: Colors.black54, fontSize: 20),
                     ),
-                  ],
-                ),
-              ),
-              Container(
-                child: Text(
-                  '내용',
-                  style: TextStyle(color: Colors.black54, fontSize: 20),
-                ),
-              )
-            ]));
+                  )
+                ])));
   }
 }
