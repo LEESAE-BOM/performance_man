e77aaba645c5358d5dc3851f4184a4f9a8ef243b
ssongdda <70890888+ssongdda@users.noreply.github.com>
2021-10-04 15:53:00 +0900

    Create outsourcing_ratio_widget.dart

---
+++ Front/lib/screens/components/management_widget/outsourcing_ratio_widget.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class Outsourcing_Ratio_Widget extends StatefulWidget {
+  @override
+  _Outsourcing_Ratio_Widget createState() => _Outsourcing_Ratio_Widget();
+}
+
+class _Outsourcing_Ratio_Widget extends State<Outsourcing_Ratio_Widget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 180,
+        width: 180,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+          border: Border.all(color: Colors.black12, width: 3),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey,
+              offset: Offset(0.0, 1.0), //(x,y)
+              blurRadius: 6.0,
+            ),
+          ],
+        ),
+        //margin: EdgeInsets.all(30),
+        child: Column(
+            children: <Widget>[
+              Container(
+                height: 50,
+                padding: EdgeInsets.only(top: 5, bottom: 5, left: 10),
+                child: Row(
+                  children: [
+                    Text(
+                      '외주비율',
+                      style: TextStyle(color: Colors.black54, fontSize: 15),
+                    ),
+                    Image.asset(
+                      'image/danger.png',
+                      width: 10,
+                      height: 10,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                child: Text(
+                  '내용',
+                  style: TextStyle(color: Colors.black54, fontSize: 20),
+                ),
+              )
+            ]));
+  }
+}
