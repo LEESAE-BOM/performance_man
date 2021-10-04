b9c929cb85354ef829eb816d7b4c6a6462bd0640
이새봄 <70834586+LEESAE-BOM@users.noreply.github.com>
2021-10-04 17:42:37 +0900

    월별수치 수정

--- Front/lib/energy_widget/monthly_figures.dart
+++ Front/lib/energy_widget/monthly_figures.dart
@@ -11,10 +11,18 @@ class monthly_figures extends StatefulWidget {
 
 class _monthly_figures extends State<monthly_figures> {
   late List<monthly_data> _monthly_data;
+  late List<monthly_data> _monthly_data2;
+  late List<monthly_data> _monthly_data3;
+  late List<monthly_data> _monthly_data4;
+
   late TooltipBehavior _toolTipBehavior;
 
   void initState() {
     _monthly_data = get_data();
+    _monthly_data2 = get_data2();
+    _monthly_data3 = get_data3();
+    _monthly_data4 = get_data4();
+
     _toolTipBehavior = TooltipBehavior();
     super.initState();
   }
@@ -29,34 +37,122 @@ class _monthly_figures extends State<monthly_figures> {
     ];
     return chartData;
   }
+  List<monthly_data> get_data2(){
+    final List<monthly_data> chartData = [
+      monthly_data('5월',24, 531),
+      monthly_data('6월',214, 12),
+      monthly_data('7월',124, 02),
+      monthly_data('8월',224, 259),
+      monthly_data('9월',244, 540)
+    ];
+    return chartData;
+  }
+  List<monthly_data> get_data3(){
+    final List<monthly_data> chartData = [
+      monthly_data('5월',24, 51),
+      monthly_data('6월',324,  152),
+      monthly_data('7월',12, 20),
+      monthly_data('8월',424, 59),
+      monthly_data('9월',24, 50)
+    ];
+    return chartData;
+  }
+  List<monthly_data> get_data4(){
+    final List<monthly_data> chartData = [
+      monthly_data('5월',24, 192),
+      monthly_data('6월',214,  152),
+      monthly_data('7월',124, 202),
+      monthly_data('8월',224, 259),
+      monthly_data('9월',244, 500)
+    ];
+    return chartData;
+  }
 
   @override
   Widget build(BuildContext context) {
 
-    Widget textSection = const Padding(
-      padding: EdgeInsets.all(32),
-      child: Text(
-        '9월의 전력 사용량은 13,488kWh'
-            '전월 대비 2,000만큼 감소 했어요.',
-        softWrap: true,
-        style: TextStyle(fontSize: 25, fontFamily: 'applesdneom'),
-        textAlign: TextAlign.center,
-      ),
+    Widget textSection = Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Text.rich(
+              TextSpan(// default text style
+                children: <TextSpan>[
+                  TextSpan(text: '9월의 전력 사용량은',
+                    style:TextStyle(
+                      fontSize:17.0,
+                      color: Colors.black,
+                      letterSpacing: 0.3,
+                      fontFamily: 'applesdneob',
+                    ),),
+                  TextSpan(text: '13,588 kWh\n',
+                    style:TextStyle(
+                        fontSize: 30.0,
+                        color: Colors.blue,
+                        letterSpacing: 5.0,
+                        // fontWeight: FontWeight.bold,
+                        fontFamily: 'applesdneob'),),
+                  TextSpan(text: '전월대비 ',
+                    style:TextStyle(
+                        fontSize: 17.0,
+                        color: Colors.black,
+                        letterSpacing: 5.0,
+                        fontFamily: 'applesdneob'),),
+                  TextSpan(text: ' 2000만큼 감소',
+                    style:TextStyle(
+                        fontSize: 30.0,
+                        color: Colors.blue,
+                        letterSpacing: 3.0,
+                        fontFamily: 'applesdneob'),),
+                  TextSpan(text: ' 했어요',
+                    style:TextStyle(
+                        fontSize: 17.0,
+                        color: Colors.black,
+                        letterSpacing: 1.0,
+                        // fontWeight: FontWeight.bold,
+                        fontFamily: 'applesdneob'),
+                  ),
+
+              ]
+            ),
+            )
+          ],
+        )
     );
+
     Widget chartSection= Center(
         child: Container(
           child: SfCartesianChart(
+              legend: Legend(
+                  isVisible: true,
+                  // Legend will be placed at the left
+                  position: LegendPosition.top)
+              ,
               primaryXAxis: CategoryAxis(),
               series: <CartesianSeries>[
                 // Render column series
                 ColumnSeries<monthly_data, String>(
+                  name:'전 년도 전력사용량',
                     dataSource: _monthly_data,
+                    xValueMapper: (monthly_data data, _) => data.x,
+                    yValueMapper: (monthly_data data, _) => data.y
+                ),
+                ColumnSeries<monthly_data, String>(
+                    name:'현 년도 전력사용량',
+                    dataSource: _monthly_data2,
                     xValueMapper: (monthly_data data, _) => data.x,
                     yValueMapper: (monthly_data data, _) => data.y
                 ),
                 // Render line series
                 LineSeries<monthly_data, String>(
-                    dataSource: _monthly_data,
+                    name:'전 년도 최대전력',
+                    dataSource: _monthly_data3,
+                    xValueMapper: (monthly_data data, _) => data.x,
+                    yValueMapper: (monthly_data data, _) => data.y1
+                ),
+                LineSeries<monthly_data, String>(
+                    name:'현 년도 최대전력',
+                    dataSource: _monthly_data4,
                     xValueMapper: (monthly_data data, _) => data.x,
                     yValueMapper: (monthly_data data, _) => data.y1
                 )
@@ -111,7 +207,7 @@ class _monthly_figures extends State<monthly_figures> {
     return MaterialApp(
       home:Scaffold(
         appBar: AppBar(
-          title: Text('노동 생산율',
+          title: Text('월별 전력 사용량',
             style: TextStyle(fontSize: 25, fontFamily: 'applesdneom'),),
           centerTitle: true,
           backgroundColor: Color.fromRGBO(43, 63, 107, 1),
@@ -122,9 +218,13 @@ class _monthly_figures extends State<monthly_figures> {
         body: Center(
           child: ListView(
             children: [
+              SizedBox(height:35),
               textSection,
+              SizedBox(height:35),
               chartSection,
-              datatableSection
+              SizedBox(height:35),
+              datatableSection,
+              SizedBox(height:15)
             ],
           ),
         ),
