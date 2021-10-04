1a531626555496773867f2a858bc20e56e381ae8
ssongdda <70890888+ssongdda@users.noreply.github.com>
2021-10-04 18:45:35 +0900

    에너지 메인 수정

--- Front/lib/screens/energy/energy_screen.dart
+++ Front/lib/screens/energy/energy_screen.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import '.././components/energy_widget/Hourly_Figures_Widget.dart';
+import '.././components/energy_widget/energy_fee_widget.dart';
+import '.././components/energy_widget/monthly_figures_widget.dart';
 
-class Energy_Screen extends StatelessWidget{
+class Energy_Screen extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return Center(
-      child: Text('energy_screen'),
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: ListView(
+          children: [
+            Container(
+                color: Colors.indigo,
+                height: 50,
+                child: Text('이번달 전력 사용량\n123,456kWh',
+                  style: TextStyle(color: Colors.black54, fontSize: 20),)
+            ),
+            Monthly_Figures_Widget(),
+            Energy_Fee_Widget(),
+            Hourly_Figures_Widget(),
+          ],
+        )
     );
   }
 }
