39f8913448b24d0afa6ee977c6406f2bfb174432
이새봄 <70834586+LEESAE-BOM@users.noreply.github.com>
2021-10-05 14:51:17 +0900

    메뉴(공장,에너지) 추가

--- Front/lib/screens/components/menu_drawer.dart
+++ Front/lib/screens/components/menu_drawer.dart
@@ -6,6 +6,14 @@ import 'package:flutter_app/management_widget/labor_ratio.dart';
 import 'package:flutter_app/management_widget/order_amount.dart';
 import 'package:flutter_app/management_widget/outsourcing_ratio.dart';
 import 'package:flutter_app/management_widget/sales.dart';
+import 'package:flutter_app/factory_widget/capacity_ratio.dart';
+import 'package:flutter_app/factory_widget/development_completion_rate.dart';
+import 'package:flutter_app/factory_widget/labor_production_rate.dart';
+import 'package:flutter_app/factory_widget/lead_time.dart';
+import 'package:flutter_app/energy_widget/monthly_figures.dart';
+import 'package:flutter_app/energy_widget/hourly_figures.dart';
+import 'package:flutter_app/energy_widget/energy_fee.dart';
+
 
 class MenuDrawer extends StatelessWidget{
   @override
@@ -73,6 +81,48 @@ class _Menu_Drawer extends State<Menu_Drawer> {
               title: Text('추정수익률'),
               onTap: (){
                 Navigator.of(context).push(MaterialPageRoute(builder: (context)=>expected_profit()));
+              },
+            ),
+            ListTile(
+              title: Text('개발완료율'),
+              onTap: (){
+                Navigator.of(context).push(MaterialPageRoute(builder: (context)=>development_completion_rate()));
+              },
+            ),
+            ListTile(
+              title: Text('설비가동율'),
+              onTap: (){
+                Navigator.of(context).push(MaterialPageRoute(builder: (context)=>capacity_ratio()));
+              },
+            ),
+            ListTile(
+              title: Text('노동생산율'),
+              onTap: (){
+                Navigator.of(context).push(MaterialPageRoute(builder: (context)=>labor_production_rate()));
+              },
+            ),
+            ListTile(
+              title: Text('리드 타임'),
+              onTap: (){
+                Navigator.of(context).push(MaterialPageRoute(builder: (context)=>lead_time()));
+              },
+            ),
+            ListTile(
+              title: Text('월별 수치'),
+              onTap: (){
+                Navigator.of(context).push(MaterialPageRoute(builder: (context)=>monthly_figures()));
+              },
+            ),
+            ListTile(
+              title: Text('시간별 수치'),
+              onTap: (){
+                Navigator.of(context).push(MaterialPageRoute(builder: (context)=>hourly_figures()));
+              },
+            ),
+            ListTile(
+              title: Text('요금현황'),
+              onTap: (){
+                Navigator.of(context).push(MaterialPageRoute(builder: (context)=>energy_fee()));
               },
             )
           ],
