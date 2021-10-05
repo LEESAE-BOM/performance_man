214d5287bbb522aba6482f315da839999930e790
이새봄 <70834586+LEESAE-BOM@users.noreply.github.com>
2021-10-05 14:56:33 +0900

    리드타임 메뉴전환 추가

--- Front/lib/factory_widget/lead_time.dart
+++ Front/lib/factory_widget/lead_time.dart
@@ -322,7 +322,7 @@ class _lead_time extends State<lead_time> {
           centerTitle: true,
           backgroundColor: Color.fromRGBO(43, 63, 107, 1),
           leading: IconButton(icon: Icon(Icons.arrow_back,color:Colors.white,), onPressed: (){
-            Factory_Screen();
+            Navigator.of(context).pop();
           }),
         ),
         body: SafeArea(
