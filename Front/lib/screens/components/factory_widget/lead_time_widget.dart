752e34be68b2cf15e2c2a6df0a07c7d28c6dd322
이새봄 <70834586+LEESAE-BOM@users.noreply.github.com>
2021-10-08 02:05:58 +0900

    수정수정

--- Front/lib/screens/components/factory_widget/lead_time_widget.dart
+++ Front/lib/screens/components/factory_widget/lead_time_widget.dart
@@ -37,55 +37,48 @@ class _Lead_Time_Widget extends State<Lead_Time_Widget> {
                 ),
                 //margin: EdgeInsets.all(30),
                 child: Column(
-                        children: <Widget>[
-                          Container(
-                            height: 50,
-                            padding: EdgeInsets.only(top: 5, bottom: 5, left: 10),
-                            child: Row(
-                              children: [
-                                Text(
-                                  'lead-time',
-                                  style: TextStyle(color: Colors.black54, fontSize: 15),
-                                ),
-                                Image.asset(
-                                  'image/safe.png',
-                                  width: 10,
-                                  height: 10,
-                                ),
-                              ],
-                            ),),
-                          Container(
-                              width: ScreenUtil().setWidth(150),
-                              height: ScreenUtil().setHeight(140),
-                              child:Column(
-                              children: [
-                                Text.rich(
-                                  TextSpan(// default text style
-                                    children: <TextSpan>[
-                                      TextSpan(text: '9',
-                                        style:TextStyle(
-                                          fontSize: 110,
-                                          color: Color.fromRGBO(43, 63, 107, 1),
-                                          letterSpacing: 5.0,
-                                          fontFamily: 'applesdneob',
-                                        ),),
-                                      TextSpan(text: 'days ',
-                                        style:TextStyle(
-                                          fontSize: 50.0,
-                                          color:  Colors.black87,
-                                          letterSpacing: 5.0,
-                                          fontFamily: 'applesdneob',),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ]
-                          )
-                          )
-                        ]
-                    )
+                    children: <Widget>[
+                      Container(
+                        height: ScreenUtil().setHeight(30),
+                        padding: EdgeInsets.only(top: 5, bottom: 5, left: 10),
+                        child: Row(
+                          children: [
+                            Text(
+                              'lead-time',
+                              style: TextStyle(color: Colors.black54, fontSize: 15),
+                            ),
+                            Image.asset(
+                              'image/safe.png',
+                              width: 10,
+                              height: 10,
+                            ),
+                          ],
+                        ),),
+                      Text.rich(
+                        TextSpan(// default text style
+                          children: <TextSpan>[
+                            TextSpan(text: '9',
+                              style:TextStyle(
+                                fontSize: 85.sp,
+                                color: Color.fromRGBO(43, 63, 107, 1),
+                                letterSpacing: 5.0,
+                                fontFamily: 'applesdneob',
+                              ),),
+                            TextSpan(text: 'days ',
+                              style:TextStyle(
+                                fontSize: 25.sp,
+                                color:  Colors.black87,
+                                letterSpacing: 5.0,
+                                fontFamily: 'applesdneob',),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ]
+
                 )
             )
+        )
     );
   }
 }
