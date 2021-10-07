42df3efa13e8b07cee3a60b6f01fc5ae335f57c4
sseunnxx <89835693+sseunnxx@users.noreply.github.com>
2021-10-07 13:36:30 +0900

    너비 수정

--- Front/lib/screens/components/management_widget/sales_widget.dart
+++ Front/lib/screens/components/management_widget/sales_widget.dart
@@ -11,50 +11,52 @@ class _Sales_Widget extends State<Sales_Widget> {
   Widget build(BuildContext context) {
     return GestureDetector(
 
-        onTap: (){
-      Navigator.of(context).push(MaterialPageRoute(builder: (context)=>sales()));
-    },
-    child:Container(
-        height: 180,
-        width: 180,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(20),
-          border: Border.all(color: Colors.black12, width: 3),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey,
-              offset: Offset(0.0, 1.0), //(x,y)
-              blurRadius: 6.0,
+        onTap: () {
+          Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) => sales()));
+        },
+        child: Container(
+            height: 100,
+            width: MediaQuery.of(context).size.width ,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(color: Colors.black12, width: 3),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  offset: Offset(0.0, 1.0), //(x,y)
+                  blurRadius: 6.0,
+                ),
+              ],
             ),
-          ],
-        ),
-        //margin: EdgeInsets.all(30),
-        child: Column(
-            children: <Widget>[
-              Container(
-                height: 50,
-                padding: EdgeInsets.only(top: 5, bottom: 5, left: 10),
-                child: Row(
-                  children: [
-                    Text(
-                      '매출금액',
-                      style: TextStyle(color: Colors.black54, fontSize: 15),
+            //margin: EdgeInsets.all(30),
+            child: Column(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.only(top: 10, bottom: 5, left: 10),
+                    child: Row(
+                      children: [
+                        Text(
+                          '매출금액',
+                          style: TextStyle(color: Colors.black54, fontSize: 15),
+                        ),
+                        Image.asset(
+                          'image/safe.png',
+                          width: 10,
+                          height: 10,
+                        ),
+                      ],
                     ),
-                    Image.asset(
-                      'image/safe.png',
-                      width: 10,
-                      height: 10,
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                child: Text(
-                  '이번달 매출금액은\n123,456,789원입니다.',
-                  style: TextStyle(color: Colors.black54, fontSize: 20),
-                ),
-              )
-            ])));
+                  ),
+                  Center(
+                      child: Text(
+                        '이번달 매출금액은\n123,456,789원입니다.',
+                        style: TextStyle(color: Colors.black54,
+                        ),
+                      )
+                  )
+                ]))
+    );
   }
 }
