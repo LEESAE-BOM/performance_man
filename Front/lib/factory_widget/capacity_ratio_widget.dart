d57f5dc5d7ad5521a70fd8a43265e0720f382828
이새봄 <70834586+LEESAE-BOM@users.noreply.github.com>
2021-10-07 22:41:51 +0900

    Add files via upload

---
+++ Front/lib/factory_widget/capacity_ratio_widget.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/factory_widget/capacity_ratio.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class Capacity_Ratio_Widget extends StatefulWidget {
+  @override
+  _Capacity_Ratio_Widget createState() => _Capacity_Ratio_Widget();
+}
+
+class _Capacity_Ratio_Widget extends State<Capacity_Ratio_Widget> {
+  late List<Chart_Data>? _chart_Data;
+  late TooltipBehavior _toolTipBehavior;
+
+  void initState() {
+    _chart_Data = getChartData();
+    _toolTipBehavior = TooltipBehavior();
+    super.initState();
+  }
+
+  List<Chart_Data>  getChartData(){
+    final List<Chart_Data>  getChartData = [
+      Chart_Data('Completed', 43, Colors.blue),
+      Chart_Data('unCompleted', 57,  Colors.grey),
+    ];
+    return getChartData;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget chartSection= Center(
+        child:
+        Container(
+            height: ScreenUtil().setHeight(150) , // Setting height and width for the circular chart annotation
+            width: ScreenUtil().setWidth(180),
+            child:
+              SfCircularChart(
+                  series: <CircularSeries>[
+                    DoughnutSeries<Chart_Data, String>(
+                        dataSource: _chart_Data,
+                        xValueMapper: (Chart_Data data, _) => data.x,
+                        yValueMapper: (Chart_Data data, _) => data.y,
+                        pointColorMapper:(Chart_Data data,  _) => data.color,
+                        dataLabelSettings: DataLabelSettings(
+                          isVisible: true,
+                        )
+                    )
+                  ]
+              )
+    )
+    );
+
+    return GestureDetector(
+        onTap: () {
+          Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) => capacity_ratio()));
+        },
+        child: Container(
+            width: ScreenUtil().setWidth(180),
+            height: ScreenUtil().setHeight(200),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(color: Colors.black12, width: 3),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  offset: Offset(0.0, 1.0), //(x,y)
+                  blurRadius: 6.0,
+                ),
+              ],
+            ),
+            //margin: EdgeInsets.all(30),
+            child: Column(
+                children: <Widget>[
+                  Container(
+                    height: 50,
+                    padding: EdgeInsets.only(top: 5, bottom: 5, left: 10),
+                    child: Row(
+                      children: [
+                        Text(
+                          '설비가동율',
+                          style: TextStyle(color: Colors.black54, fontSize: 15),
+                        ),
+                        Image.asset(
+                          'image/danger.png',
+                          width: 10,
+                          height: 10,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child:chartSection
+                  )
+                ])));
+  }
+}
+class Chart_Data {
+  Chart_Data(this.x, this.y, this.color);
+  final String x;
+  final double y;
+  final Color color;
+}
