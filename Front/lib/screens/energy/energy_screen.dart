3f040a538708a1a4572a6fbdd11d8fe57c521d86
ssongdda <70890888+ssongdda@users.noreply.github.com>
2021-10-08 13:02:50 +0900

    에너지 메인 상단 수정

--- Front/lib/screens/energy/energy_screen.dart
+++ Front/lib/screens/energy/energy_screen.dart
@@ -9,19 +9,54 @@ class Energy_Screen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: ListView(
-          children: [
-            Container(
-                alignment: Alignment.center,
-                color: Color.fromRGBO(43, 63, 107, 1),
-                height: 50,
-                child: Text('이번달 전력 사용량\n123,456kWh',
-                  style: TextStyle(color: Colors.black54, fontSize: 20),)
+      children: [
+        Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(30),
+                  bottomRight: Radius.circular(30)),
+              color: Color.fromRGBO(43, 63, 107, 1),
             ),
-            Monthly_Figures_Widget(),
-        Row (children: [Energy_Fee_Widget(),
-            Hourly_Figures_Widget(),],),
+            alignment: Alignment.center,
+            height: 180,
+            child: //Text('목표대비 총 105% 달성', style: TextStyle(color: Colors.black54, fontSize: 20),)
+                RichText(
+              text: TextSpan(children: <TextSpan>[
+                TextSpan(
+                  text: '이번 달 전력 사용량\n',
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.white,
+                    letterSpacing: 1,
+                    fontFamily: 'applesdnoeb',
+                  ),
+                ),
+                TextSpan(
+                  text: '13,588',
+                  style: TextStyle(
+                      fontSize: 40,
+                      color: Colors.white,
+                      letterSpacing: 5,
+                      fontFamily: 'applesdneob'),
+                ),
+                TextSpan(
+                  text: ' kWh',
+                  style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.white,
+                      letterSpacing: 1.0,
+                      fontFamily: 'applesdneob'),
+                ),
+              ]),
+            )),
+        Monthly_Figures_Widget(),
+        Row(
+          children: [
+            Energy_Fee_Widget(),
+            Hourly_Figures_Widget(),
           ],
-        )
-    );
+        ),
+      ],
+    ));
   }
 }
