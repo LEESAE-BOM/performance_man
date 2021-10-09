0336e246bb08588489c1d5a07945a5ef1692568d
이새봄 <70834586+LEESAE-BOM@users.noreply.github.com>
2021-10-09 17:32:38 +0900

    글자 크기조절 추가,패딩 비율 조정 ,통합

--- Front/lib/management_widget/outsourcing_ratio.dart
+++ Front/lib/management_widget/outsourcing_ratio.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:intl/intl.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class outsourcing_ratio extends StatefulWidget {
   @override
@@ -17,101 +18,83 @@ class _outsourcing_ratio extends State<outsourcing_ratio> {
   @override
   Widget build(BuildContext context) {
     Widget textSection = Padding(
-        padding: EdgeInsets.all(5),
+        padding: EdgeInsets.fromLTRB(50.sp, 100.sp, 20.sp, 100.sp),
         child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Text.rich(TextSpan(
-                  text: '전체 ',
-                  style: TextStyle(
-                    fontSize: 25.0,
-                    letterSpacing: 2.0,
-                    fontFamily: 'applesdneoeb',
-                    color: Colors.black,
-                  ),
                   children: <TextSpan>[
+                    TextSpan(
+                        text: '전체 ',
+                        style: TextStyle(
+                          fontSize: 65.sp,
+                          letterSpacing: 2.0,
+                          fontFamily: 'applesdneoeb',
+                          color: Colors.black,
+                        )),
                     TextSpan(
                       text: '수주 비용 ',
                       style: TextStyle(
-                          fontSize: 30.0,
+                          fontSize: 90.sp,
                           color: Colors.blue,
                           fontFamily: 'applesdneoeb',
                           letterSpacing: 2.0),
                     ),
                     TextSpan(
-                      text: '중 ',
+                      text: '중\n',
                       style: TextStyle(
-                        fontSize: 25.0,
+                        fontSize: 65.sp,
                         letterSpacing: 2.0,
                         fontFamily: 'applesdneoeb',
                         color: Colors.black,
                       ),
                     ),
+                    TextSpan(
+                      text: '외주 비율',
+                      style: TextStyle(
+                          fontSize: 90.sp,
+                          color: Colors.blue,
+                          fontFamily: 'applesdneoeb',
+                          letterSpacing: 2.0),
+                    ),
+                    TextSpan(
+                      text: '이 차지하는 비율은\n',
+                      style: TextStyle(
+                        fontSize: 65.sp,
+                        letterSpacing: 2.0,
+                        fontFamily: 'applesdneoeb',
+                        color: Colors.black,
+                      ),
+                    ),
+                    TextSpan(
+                      text: '약 45% ',
+                      style: TextStyle(
+                          fontSize: 90.sp,
+                          color: Colors.blue,
+                          fontFamily: 'applesdneoeb',
+                          letterSpacing: 2.0),
+                    ),
+                    TextSpan(
+                        text: '이에요.',
+                        style: TextStyle(
+                          fontSize: 65.sp,
+                          letterSpacing: 2.0,
+                          fontFamily: 'applesdneoeb',
+                          color: Colors.black,
+                        )),
+
                   ]))
             ]));
 
-    Widget textSection1 = Padding(
-        padding: EdgeInsets.all(5),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text.rich(TextSpan(text: '', children: <TextSpan>[
-              TextSpan(
-                text: '외주 비율',
-                style: TextStyle(
-                    fontSize: 30.0,
-                    color: Colors.blue,
-                    fontFamily: 'applesdneoeb',
-                    letterSpacing: 2.0),
-              ),
-              TextSpan(
-                text: '이 차지하는 비율은 ',
-                style: TextStyle(
-                  fontSize: 25.0,
-                  letterSpacing: 2.0,
-                  fontFamily: 'applesdneoeb',
-                  color: Colors.black,
-                ),
-              ),
-            ]))
-          ],
-        ));
-    Widget textSection2 = Padding(
-        padding: EdgeInsets.all(5),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text.rich(TextSpan(text: '', children: <TextSpan>[
-              TextSpan(
-                text: '약 45% ',
-                style: TextStyle(
-                    fontSize: 30.0,
-                    color: Colors.blue,
-                    fontFamily: 'applesdneoeb',
-                    letterSpacing: 2.0),
-              ),
-              TextSpan(
-                text: '이에요.',
-                style: TextStyle(
-                  fontSize: 25.0,
-                  letterSpacing: 2.0,
-                  fontFamily: 'applesdneoeb',
-                  color: Colors.black,
-                ),
-              ),
-            ]))
-          ],
-        ));
-
     Widget chartSection = Center(
         child: Container(
-            width: 500.0,
-            height: 500.0,
+            width: 1000.w,
+            height: 300,
             child: SfCircularChart(
                 title: ChartTitle(
                     text: '2021',
                     textStyle:
-                        TextStyle(fontSize: 35.0, fontWeight: FontWeight.bold)),
+                    TextStyle(fontSize: 35.0, fontWeight: FontWeight.bold)),
                 legend: Legend(
                     isVisible: true,
                     // Legend will be placed at the left
@@ -125,101 +108,105 @@ class _outsourcing_ratio extends State<outsourcing_ratio> {
                       dataLabelSettings: DataLabelSettings(
                           isVisible: true,
                           // Positioning the data label
-                          labelPosition: ChartDataLabelPosition.outside)),
+                          labelPosition: ChartDataLabelPosition.outside)
+                      ,radius: '90%'
+                  )
                 ])));
 
-    Widget datatableSection = Center(
-      child: Container(
-        width: double.infinity,
-        child: Theme(
-          data: Theme.of(context).copyWith(dividerColor: Colors.grey),
-          child: DataTable(
-            showBottomBorder: true,
-            headingRowColor:
+    Widget datatableSection = Padding(
+        padding:  EdgeInsets.fromLTRB(40.sp, 100.sp,40.sp, 0),
+        child:Center(
+          child: Container(
+            width: double.infinity,
+            child: Theme(
+              data: Theme.of(context).copyWith(dividerColor: Colors.grey),
+              child: DataTable(
+                showBottomBorder: true,
+                headingRowColor:
                 MaterialStateColor.resolveWith((states) => Colors.black12),
-            columns: <DataColumn>[
-              DataColumn(
-                label: Container(
-                  child: Text(
-                    '',
-                  ),
-                ),
-              ),
-              DataColumn(
-                label: Container(
-                  alignment: Alignment.center,
-                  child: Text('외주 내역',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          fontSize: 15.0,
-                          color: Colors.black54,
-                          fontFamily: 'applesdneoeb')),
-                ),
-              ),
-              DataColumn(
-                label: Container(
-                  child: Text(
-                    '',
-                  ),
-                ),
-              ),
-            ],
-            rows: <DataRow>[
-              DataRow(
-                cells: <DataCell>[
-                  DataCell(Text('')),
-                  DataCell(Text('')),
-                  DataCell(Container(
+                columns: <DataColumn>[
+                  DataColumn(
+                    label: Container(
                       child: Text(
-                    '최근 3개월',
-                    textAlign: TextAlign.right,
-                    style:
-                    TextStyle(
-                        fontSize: 15.0,
-                        color: Colors.black54,
-                        fontFamily: 'applesdneoeb'),
-                  )))
-                ],
-              ),
-              DataRow(
-                cells: <DataCell>[
-                  DataCell(
-                    Container(
-                      child: Text(
-                        '#,###,###',
-                        textAlign: TextAlign.right,
-                        style: TextStyle(
-                            fontSize: 15.0,
-                            color: Colors.black54,
-                            fontFamily: 'applesdneoeb'),
+                        '',
                       ),
                     ),
                   ),
-                  DataCell(
-                    Container(
-                        child: Text(
-                      '##사',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          fontSize: 15.0,
-                          color: Colors.black54,
-                          fontFamily: 'applesdneoeb'),
-                    )),
+                  DataColumn(
+                    label: Container(
+                      alignment: Alignment.center,
+                      child: Text('외주 내역',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: 43.sp,
+                              color: Colors.black54,
+                              fontFamily: 'applesdneoeb')),
+                    ),
                   ),
-                  DataCell(Text('')),
+                  DataColumn(
+                    label: Container(
+                      child: Text(
+                        '',
+                      ),
+                    ),
+                  ),
+                ],
+                rows: <DataRow>[
+                  DataRow(
+                    cells: <DataCell>[
+                      DataCell(Text('')),
+                      DataCell(Text('')),
+                      DataCell(Container(
+                          child: Text(
+                            '최근 3개월',
+                            textAlign: TextAlign.right,
+                            style:
+                            TextStyle(
+                                fontSize: 43.sp,
+                                color: Colors.black54,
+                                fontFamily: 'applesdneoeb'),
+                          )))
+                    ],
+                  ),
+                  DataRow(
+                    cells: <DataCell>[
+                      DataCell(
+                        Container(
+                          child: Text(
+                            '#,###,###',
+                            textAlign: TextAlign.right,
+                            style: TextStyle(
+                                fontSize: 43.sp,
+                                color: Colors.black54,
+                                fontFamily: 'applesdneoeb'),
+                          ),
+                        ),
+                      ),
+                      DataCell(
+                        Container(
+                            child: Text(
+                              '##사',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  fontSize: 43.sp,
+                                  color: Colors.black54,
+                                  fontFamily: 'applesdneoeb'),
+                            )),
+                      ),
+                      DataCell(Text('')),
+                    ],
+                  ),
                 ],
               ),
-            ],
-          ),
-        ),
-      ),
+            ),
+          ),)
     );
 
     return Scaffold(
       appBar: AppBar(
         title: Text(
           '외주 비율',
-          style: TextStyle(fontSize: 25.0, color: Colors.white),
+          style: TextStyle(fontSize: 67.sp, color: Colors.white),
         ),
         centerTitle: true,
         backgroundColor: Color.fromRGBO(43, 63, 107, 1),
@@ -233,24 +220,15 @@ class _outsourcing_ratio extends State<outsourcing_ratio> {
             }),
       ),
       body: SafeArea(
-          child: Padding(
-        padding: EdgeInsets.fromLTRB(15.0, 15.0, 15.0, 0),
-        child: ListView(
-          children: <Widget>[
-            textSection,
-            textSection1,
-            textSection2,
-            SizedBox(
-              height: 30.0,
+          child:Center(
+            child: ListView(
+              children: <Widget>[
+                textSection,
+                chartSection,
+                datatableSection
+              ],
             ),
-            chartSection,
-            SizedBox(
-              height: 30.0,
-            ),
-            datatableSection
-          ],
-        ),
-      )),
+          )),
     );
   }
 }
