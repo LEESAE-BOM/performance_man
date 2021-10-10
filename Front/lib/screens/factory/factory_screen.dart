66e61767b6be6be9d388e86559787226684b8e64
ssongdda <70890888+ssongdda@users.noreply.github.com>
2021-10-11 01:42:32 +0900

    공장 메인 상단 수정

--- Front/lib/screens/factory/factory_screen.dart
+++ Front/lib/screens/factory/factory_screen.dart
@@ -20,46 +20,56 @@ class Factory_Screen extends StatelessWidget {
               color: Color.fromRGBO(43, 63, 107, 1),
             ),
             alignment: Alignment.center,
-            height: 180,
-            child: //Text('목표대비 총 105% 달성', style: TextStyle(color: Colors.black54, fontSize: 20),)
-                RichText(
+            height: 350.w,
+            child: RichText(
               text: TextSpan(children: <TextSpan>[
                 TextSpan(
                   text: '개발완료\n',
                   style: TextStyle(
-                    fontSize: 30,
+                    fontSize: 65.w,
                     color: Colors.white,
-                    letterSpacing: 1,
-                    fontFamily: 'applesdnoeb',
+                    letterSpacing: 1.w,
+                    fontFamily: 'applesdneob',
                   ),
                 ),
-
                 TextSpan(
                   text: '72%',
                   style: TextStyle(
-                      fontSize: 40,
+                      fontSize: 130.w,
                       color: Colors.white,
-                      letterSpacing: 5,
+                      letterSpacing: 5.w,
                       fontFamily: 'applesdneob'),
                 ),
                 TextSpan(
                   text: ' 달성',
                   style: TextStyle(
-                      fontSize: 30,
+                      fontSize: 65.w,
                       color: Colors.white,
-                      letterSpacing: 1.0,
+                      letterSpacing: 1.w,
                       fontFamily: 'applesdneob'),
                 ),
               ]),
             )),
-            Padding ( padding: EdgeInsets.all(10.w),child :Developmentcompletion_Rate_Widget(),),
+        Padding(
+          padding: EdgeInsets.all(10.w),
+          child: Developmentcompletion_Rate_Widget(),
+        ),
         Row(
           children: [
-    Padding ( padding: EdgeInsets.all(10.w),child :Capacity_Ratio_Widget(),),
-    Padding ( padding: EdgeInsets.all(10.w),child :Lead_Time_Widget(),),
+            Padding(
+              padding: EdgeInsets.all(10.w),
+              child: Capacity_Ratio_Widget(),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10.w),
+              child: Lead_Time_Widget(),
+            ),
           ],
         ),
-    Padding ( padding: EdgeInsets.all(10.w),child :Labor_Production_Rate_Widget(),),
+        Padding(
+          padding: EdgeInsets.all(10.w),
+          child: Labor_Production_Rate_Widget(),
+        ),
       ],
     ));
   }
