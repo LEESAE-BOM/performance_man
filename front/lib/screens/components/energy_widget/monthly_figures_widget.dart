8cddb1098268f67f2540977de1c30e6faeddba49
sseunnxx <89835693+sseunnxx@users.noreply.github.com>
2021-11-06 03:30:15 +0900

    월간 사용량 위젯화면 수정

--- front/lib/screens/components/energy_widget/monthly_figures_widget.dart
+++ front/lib/screens/components/energy_widget/monthly_figures_widget.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/energy_widget/monthly_figures.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_app/mysql_connect.dart';
+import 'package:flutter_app/theme.dart';
+import 'package:flutter_app/box_widget.dart';
 //큰 위젯
 
 class Monthly_Figures_Widget extends StatefulWidget {
@@ -12,97 +14,34 @@ class Monthly_Figures_Widget extends StatefulWidget {
 class _Monthly_Figures_Widget extends State<Monthly_Figures_Widget> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        width: 1040.w,
-        height: 400.w,
-        child: GestureDetector(
-            onTap: () {
-              Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => monthly_figures()));
-            },
-            child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20),
-                  border: Border.all(color: Colors.black12),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey,
-                      offset: Offset(0.0, 1.0), //(x,y)
-                      blurRadius: 6.0,
-                    ),
-                  ],
-                ),
-                child: Column(children: <Widget>[
-                  Container(
-                    padding:
-                        EdgeInsets.only(top: 20.w, bottom: 10.w, left: 35.w),
-                    child: Row(
-                      children: [
-                        Text(
-                          '월별 에너지',
-                          style: TextStyle(
-                              color: Colors.black54,
-                              fontSize: 35.w,
-                              fontFamily: 'applesdneom'),
-                        ),
-                        SizedBox(width: 10.w),
-                        Image.asset(
-                          'image/safe.png',
-                          width: 20.w,
-                          height: 20.w,
-                        ),
-                      ],
-                    ),
-                  ),
-                  /*SizedBox(
-                    height: 10,
-                  ),*/
-                  Container(
-                    width: 1040.w,
-                    height: 310.w,
-                    alignment: Alignment.center,
-                    child: Center(
-                      child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget>[
-                            Text.rich(
-                              TextSpan(
-                                // default text style
-                                children: <TextSpan>[
-                                  TextSpan(
-                                    text: '전월대비',
-                                    style: TextStyle(
-                                      fontSize: 60.w,
-                                      color: Colors.black,
-                                      letterSpacing: 1.w,
-                                      fontFamily: 'applesdneob',
-                                    ),
-                                  ),
-                                  TextSpan(
-                                    text: '\n2000만큼',
-                                    style: TextStyle(
-                                        fontSize: 85.w,
-                                        color: Colors.blue,
-                                        letterSpacing: 5.w,
-                                        fontFamily: 'applesdneob'),
-                                  ),
-                                  TextSpan(
-                                    text: ' 감소',
-                                    style: TextStyle(
-                                        fontSize: 60.w,
-                                        color: Colors.black,
-                                        letterSpacing: 1.w,
-                                        fontFamily: 'applesdneob'),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            Icon(CupertinoIcons.hand_thumbsdown_fill,
-                                size: 160.w, color: Colors.blue)
-                          ]),
-                    ),
-                  )
-                ]))));
+    return BoxWidget('월별 에너지', 'safe', 'wide').make(
+        onTap: () {
+          Navigator.of(context).push(MaterialPageRoute(builder: (context) => monthly_figures()));
+        },
+        dbRelatedContentBuilder: FutureBuilder(
+            future: conn.sendQuery('SELECT Money * 1000 as Money FROM Money WHERE MoneyCategory = \'EGFEE\' ORDER BY MoneyDate ASC;'),
+            builder: (context, snapshot){
+              if(snapshot.hasData){
+                var result = snapshot.data as List<Map<String, dynamic>>;
+                var thisMonthPrice = double.parse(result[0]['Money']);
+                var previousMonthPrice = double.parse(result[1]['Money']);
+
+                int diff = thisMonthPrice.round() - previousMonthPrice.round();
+                return Text.rich(
+                    TextSpan(
+                        children: [
+                          detailPageTheme.makeHeaderText('전월 대비\n'),
+                          if(diff < 0)
+                            detailPageTheme.makeHeaderText('[${detailPageTheme.money.format(diff * -1)}]kWh 감소'),
+                          if(diff >= 0)
+                            detailPageTheme.makeHeaderText('[$diff]kWh 증가')
+                        ]
+                    ));
+              }else{
+                return Text.rich(TextSpan(text: '불러오는 중'));
+              }
+            }
+        )
+    );
   }
 }
