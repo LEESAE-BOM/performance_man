efca5c5bc7c8d885fe896cac95027cecf54725c1
sseunnxx <89835693+sseunnxx@users.noreply.github.com>
2021-11-06 21:43:53 +0900

    디비연결

--- front/lib/screens/components/energy_widget/energy_fee_widget.dart
+++ front/lib/screens/components/energy_widget/energy_fee_widget.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/energy_widget/energy_fee.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_app/mysql_connect.dart';
+import 'package:flutter_app/theme.dart';
+import 'package:flutter_app/box_widget.dart';
 
 //작은 위젯
 class Energy_Fee_Widget extends StatefulWidget {
@@ -11,79 +13,27 @@ class Energy_Fee_Widget extends StatefulWidget {
 class _Energy_Fee_Widget extends State<Energy_Fee_Widget> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        width: 510.w,
-        height: 400.w,
-        child: GestureDetector(
-            onTap: () {
-              Navigator.of(context)
-                  .push(MaterialPageRoute(builder: (context) => energy_fee()));
-            },
-            child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20),
-                  border: Border.all(color: Colors.black12),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey,
-                      offset: Offset(0.0, 1.0), //(x,y)
-                      blurRadius: 6.0,
-                    ),
-                  ],
-                ),
-                child: Column(children: <Widget>[
-                  Container(
-                    padding:
-                        EdgeInsets.only(top: 20.w, left: 35.w),
-                    child: Row(
-                      children: [
-                        Text(
-                          '요금현황',
-                          style: TextStyle(
-                              color: Colors.black54,
-                              fontSize: 35.w,
-                              fontFamily: 'applesdneom'),
-                        ),
-                        SizedBox(width: 10.w),
-                        Image.asset(
-                          'image/safe.png',
-                          width: 20.w,
-                          height: 20.w,
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    width: 510.w,
-                    height: 310.w,
-                    child: Center(
-                      child: Text.rich(
-                        TextSpan(
-                          // default text style
-                          children: <TextSpan>[
-                            TextSpan(
-                              text: '300',
-                              style: TextStyle(
-                                fontSize: 120.w,
-                                color: Colors.blue,
-                                letterSpacing: 5.w,
-                                fontFamily: 'applesdneob',
-                              ),
-                            ),
-                            TextSpan(
-                              text: ' kWh',
-                              style: TextStyle(
-                                  fontSize: 70.w,
-                                  color: Colors.black,
-                                  letterSpacing: 1.w,
-                                  fontFamily: 'applesdneob'),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  )
-                ]))));
+    return BoxWidget('요금 현황', 'safe', 'wide').make(
+        onTap: () {
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => energy_fee()));
+        },
+        dbRelatedContentBuilder: FutureBuilder(
+            future: conn.sendQuery(
+                'SELECT MoneyDate, Money * 1000 as Money FROM Money WHERE MoneyCategory=\'EGFEE\' ORDER BY MoneyDate DESC;'),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                var result = snapshot.data as List<Map<String, dynamic>>;
+                var thisMonthPrice = double.parse(result[0]['Money']);
+                var previousMonthPrice = double.parse(result[1]['Money']);
+
+                return Text.rich(TextSpan(children: [
+                  detailPageTheme.makeHeaderText(
+                      '이번 달 전기요금 현황은\n[${detailPageTheme.money.format(thisMonthPrice)}원]'),
+                ]));
+              } else {
+                return Text.rich(TextSpan(text: '불러오는 중'));
+              }
+            }));
   }
 }
