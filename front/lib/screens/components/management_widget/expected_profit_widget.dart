b3774fa86adc115fde4e78efc3c549a70797946a
이새봄 <70834586+LEESAE-BOM@users.noreply.github.com>
2021-11-11 04:37:46 +0900

    분기별 예상 수익 구현

--- front/lib/screens/components/management_widget/expected_profit_widget.dart
+++ front/lib/screens/components/management_widget/expected_profit_widget.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/management_widget/expected_profit.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ml_algo/ml_algo.dart';
+import 'package:ml_dataframe/ml_dataframe.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
+late List<double> dataset1=[0,0,0,0,0,0,0,0,0,0,0,0];
 //큰 위젯
 class Expected_Profit_Widget extends StatefulWidget {
   @override
@@ -10,19 +14,24 @@ class Expected_Profit_Widget extends StatefulWidget {
 }
 
 class _Expected_Profit_Widget extends State<Expected_Profit_Widget> {
+
+  void initState(){
+    machine().then((val) {
+      setState(() {});
+    }).catchError((error, stackTrace) {
+      print("outer: $error");
+    });
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     final List<SalesData> chartdata = [
-      SalesData('1월', 2.0),
-      SalesData('2월', 2.8),
-      SalesData('3월', 3.6),
-      SalesData('4월', 3.0),
-      SalesData('5월', 4.5),
-      SalesData('6월', 4.0),
-      SalesData('7월', 4.2),
-      SalesData('8월', 3.8),
-      SalesData('9월', 3.9)
+      SalesData('1분기', dataset1[0]+dataset1[1]+dataset1[2]),
+      SalesData('2분기',dataset1[3]+dataset1[4]+dataset1[5] ),
+      SalesData('3분기', dataset1[6]+dataset1[7]+dataset1[8]),
+      SalesData('4분기', dataset1[9]+dataset1[10]+dataset1[11])
     ];
+
     Widget chartSection = Center(
       child: Container(
         child: SfCartesianChart(
@@ -49,8 +58,8 @@ class _Expected_Profit_Widget extends State<Expected_Profit_Widget> {
 
     return GestureDetector(
         onTap: () {
-          Navigator.of(context)
-              .push(MaterialPageRoute(builder: (context) => expected_profit()));
+         Navigator.of(context).push(MaterialPageRoute(builder: (context) => expected_profit()));
+          //Navigator.of(context).pushNamed(,arguments: dataset);
         },
         child: Container(
             width: 1040.w,
@@ -98,4 +107,66 @@ class SalesData {
 
   final String? month;
   final double sales;
+}
+Future<void> machine() async {
+  final unlabel = DataFrame(<Iterable>[
+    ['month', 'profit'],
+    [1,],
+    [2,],
+    [3,],
+    [4,],
+    [5,],
+    [6,],
+    [7,],
+    [8,],
+    [9,],
+    [10,],
+    [11,],
+    [12,],
+  ], headerExists: true);
+
+  //final samples = DataFrame(data, headerExists: true);
+  final rawCsvContent = await rootBundle.loadString(
+      'dataset/machine_sample_2016.csv');
+  final samples_2016 = DataFrame.fromRawCsv(rawCsvContent, headerExists: true);
+
+  final rawCsvContent2 = await rootBundle.loadString(
+      'dataset/machine_sample_2017.csv');
+  final samples_2017 = DataFrame.fromRawCsv(rawCsvContent2, headerExists: true);
+
+  final rawCsvContent3 = await rootBundle.loadString(
+      'dataset/machine_sample_2018.csv');
+  final samples_2018 = DataFrame.fromRawCsv(rawCsvContent3, headerExists: true);
+
+  final rawCsvContent4 = await rootBundle.loadString(
+      'dataset/machine_sample_2019.csv');
+  final samples_2019 = DataFrame.fromRawCsv(rawCsvContent4, headerExists: true);
+
+  final rawCsvContent5 = await rootBundle.loadString(
+      'dataset/machine_sample_2020.csv');
+  final samples_2020 = DataFrame.fromRawCsv(rawCsvContent5, headerExists: true);
+
+  final targetName = 'profit';
+  final knnregressor= KnnRegressor(samples_2016, targetName, 3);
+
+  final regressor_2 = knnregressor.retrain(samples_2017);
+  //print('Regression coefficients: ${regressor_2.coefficients}');
+
+  final regressor_3 = regressor_2.retrain(samples_2018);
+  //print('Regression coefficients: ${regressor_3.coefficients}');
+
+  final regressor_4 = regressor_3.retrain(samples_2019);
+  //print('Regression coefficients: ${regressor_4.coefficients}');
+
+  final regressor_5 = regressor_4.retrain(samples_2020);
+  //print('Regression coefficients: ${regressor_5.coefficients}');
+
+  final kprediction = regressor_5.predict(unlabel);
+  print(kprediction);
+  print(kprediction.rows);
+
+  for(int k=0;k<12;k++) {
+    String i = kprediction.rows.elementAt(k).toString();
+    dataset1[k] = double.parse(i.substring(1, i.length - 1));
+  }
 }
