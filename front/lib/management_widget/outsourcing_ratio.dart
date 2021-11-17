6d007e7c85d376d48948ec0c6350d31b30c3ce26
송다인 <70890888+ssongdda@users.noreply.github.com>
2021-11-17 16:58:35 +0900

    파이 차트 단위 수정

--- front/lib/management_widget/outsourcing_ratio.dart
+++ front/lib/management_widget/outsourcing_ratio.dart
@@ -33,89 +33,86 @@ class _outsourcing_ratio extends State<outsourcing_ratio> {
             }),
       ),
       body: SafeArea(
-          child:Center(
-            child: FutureBuilder(
-                future: conn.sendQuery('SELECT ContractDate, Company, Price * 1000 as Money, Outsourcing * 1000 as OS FROM Contract ORDER BY ContractDate DESC;'),
-                builder: (context, snapshot){
-                  if(snapshot.hasData) {
-                    var result = snapshot.data as List<Map<String, dynamic>>;
-                    var table = ResultSet(result, ['날짜', '기업', '계약금액', '외주금액']);
-                    double contractPrice = 0;
-                    double outsourcePrice = 0;
-                    double rate = 0;
-                    int thisYear = DateTime.now().year;
+          child: Center(
+        child: FutureBuilder(
+            future: conn.sendQuery(
+                'SELECT ContractDate, Company, Price * 1000 as Money, Outsourcing * 1000 as OS FROM Contract ORDER BY ContractDate DESC;'),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                var result = snapshot.data as List<Map<String, dynamic>>;
+                var table = ResultSet(result, ['날짜', '기업', '계약금액', '외주금액']);
+                double contractPrice = 0;
+                double outsourcePrice = 0;
+                double rate = 0;
+                int thisYear = DateTime.now().year;
 
-                    for (var row in result) {
-                      contractPrice += double.parse(row['Money']);
-                      outsourcePrice += double.parse(row['OS']);
-                    }
+                for (var row in result) {
+                  contractPrice += double.parse(row['Money']);
+                  outsourcePrice += double.parse(row['OS']);
+                }
 
-                    rate = (outsourcePrice / contractPrice) * 100;
+                rate = (outsourcePrice / contractPrice) * 100;
 
-                    outsourcingData.add(ChartData('외주비용', outsourcePrice));
-                    outsourcingData.add(ChartData(' ', contractPrice - outsourcePrice));
+                outsourcingData.clear();
+                outsourcingData.add(ChartData('외주비용', rate.round()));
+                outsourcingData.add(ChartData('자체제작', 100 - rate.round()));
 
-                    return ListView(
-                      children: [
-                        Padding(
-                            padding: EdgeInsets.fromLTRB(80.sp, 100.sp, 20.sp, 100.sp),
-                            child: Text.rich(
-                                TextSpan(
-                                    children: [
-                                      detailPageTheme.makeHeaderText('전체 수주 비용 중\n[외주 비용]이 차지하는 비율은\n[약 ${rate.round()}%]예요.'),
-                                    ]
-                                )
-                            )
-                        ),
-                        Container(
-                            width: 1000.w,
-                            height: 300,
-                            child: SfCircularChart(
-                                title: ChartTitle(
-                                    text: '$thisYear',
-                                    textStyle:
-                                    TextStyle(fontSize: 35.0, fontWeight: FontWeight.bold)),
-                                legend: Legend(
-                                    isVisible: true,
-                                    position: LegendPosition.bottom,
-                                    isResponsive: false
-                                ),
-                                series: <CircularSeries>[
-                                  PieSeries<ChartData, String>(
-                                      dataSource: outsourcingData,
-                                      xValueMapper: (ChartData data, _) => data.x,
-                                      yValueMapper: (ChartData data, _) => data.y,
-                                      dataLabelSettings: DataLabelSettings(
-                                          isVisible: true,
-                                          // Positioning the data label
-                                          labelPosition: ChartDataLabelPosition.outside),
-                                      radius: '90%'
-                                  )
-                                ]
-                            )
-                        ),
-                        Table(
-                          border: detailPageTheme.tableBorderStyle,
-                          children: <TableRow>[
-                            table.getTableHeaderWidget()
-                          ] + table.getTableRowWidgets(
-                              convertor: (row){
-                            row['Money'] = '${detailPageTheme.money.format(double.parse(row['Money']))} 원';
-                            row['OS'] = '${detailPageTheme.money.format(double.parse(row['OS']))} 원';
+                return ListView(
+                  children: [
+                    Padding(
+                        padding:
+                            EdgeInsets.fromLTRB(80.sp, 100.sp, 20.sp, 100.sp),
+                        child: Text.rich(TextSpan(children: [
+                          detailPageTheme.makeHeaderText(
+                              '전체 수주 비용 중\n[외주 비용]이 차지하는 비율은\n[약 ${rate.round()}%]예요.'),
+                        ]))),
+                    Container(
+                        width: 1000.w,
+                        height: 300,
+                        child: SfCircularChart(
+                            title: ChartTitle(
+                                text: '$thisYear',
+                                textStyle: TextStyle(
+                                    fontSize: 35.0,
+                                    fontWeight: FontWeight.bold)),
+                            legend: Legend(
+                                isVisible: true,
+                                position: LegendPosition.bottom,
+                                isResponsive: false),
+                            series: <CircularSeries>[
+                              PieSeries<ChartData, String>(
+                                  radius: '100%',
+                                  dataSource: outsourcingData,
+                                  xValueMapper: (ChartData data, _) => data.x,
+                                  yValueMapper: (ChartData data, _) => data.y,
+                                  dataLabelMapper: (ChartData data, _) =>
+                                      '${data.y}%',
+                                  dataLabelSettings: DataLabelSettings(
+                                      isVisible: true,
+                                      textStyle: TextStyle(
+                                          fontSize: 50.w,
+                                          fontFamily: 'applesdneob')))
+                            ])),
+                    Table(
+                      border: detailPageTheme.tableBorderStyle,
+                      children: <TableRow>[table.getTableHeaderWidget()] +
+                          table.getTableRowWidgets(convertor: (row) {
+                            row['Money'] =
+                                '${detailPageTheme.money.format(double.parse(row['Money']))} 원';
+                            row['OS'] =
+                                '${detailPageTheme.money.format(double.parse(row['OS']))} 원';
                             return row;
                           }),
-                        )
-                      ],
-                    );
-                  } else if(snapshot.hasError) {
-                    return Text('${snapshot.error}');
-                  } else {
-                    return Text('불러오는 중');
-                  }
-                }
-            ),
-          )
-      ),
+                    )
+                  ],
+                );
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              } else {
+                return Text('불러오는 중');
+              }
+            }),
+      )),
     );
   }
 }
@@ -124,5 +121,5 @@ class ChartData {
   ChartData(this.x, this.y);
 
   String x;
-  double y;
+  int y;
 }
