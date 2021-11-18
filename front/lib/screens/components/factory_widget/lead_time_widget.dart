63eda2eac4db7fb73aa32c6d1aad5409ea9e4041
sseunnxx <89835693+sseunnxx@users.noreply.github.com>
2021-11-18 23:32:14 +0900

    안전위험경고 연결

--- front/lib/screens/components/factory_widget/lead_time_widget.dart
+++ front/lib/screens/components/factory_widget/lead_time_widget.dart
@@ -11,32 +11,49 @@ class Lead_Time_Widget extends StatefulWidget {
 }
 
 class _Lead_Time_Widget extends State<Lead_Time_Widget> {
+  var state = 'danger';
+
   @override
   Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: conn.sendQuery(
+            'SELECT DueDate as dueDate FROM LeadTime ORDER BY DueDate ASC;'),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            var result = snapshot.data as List<Map<String, dynamic>>;
+            var today = DateTime.now();
+            int dif = int.parse(today
+                .difference(DateTime.parse(result[0]['dueDate']))
+                .inDays
+                .toString());
+            dif *= -1;
 
-    return BoxWidget('제조 Lead-time', 'safe', 'narrow').make(
-        onTap: () {
-          Navigator.of(context)
-              .push(MaterialPageRoute(builder: (context) => lead_time()));
-        },
-        dbRelatedContentBuilder: FutureBuilder(
-            future: conn.sendQuery(
-                'SELECT DueDate as dueDate FROM LeadTime ORDER BY DueDate ASC;'),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                var result = snapshot.data as List<Map<String, dynamic>>;
-                var today = DateTime.now();
-                int dif=int.parse(today.difference(DateTime.parse(result[0]['dueDate'])).inDays.toString());
-                dif*=-1;
+            if (dif < 10)
+              state = 'danger';
+            else if (dif < 20)
+              state = 'warning';
+            else
+              state = 'safe';
 
-                return Text.rich(
-                    detailPageTheme.makeHeaderText('[${dif}]days]'));
-              } else {
-                return Text.rich(TextSpan(text: '불러오는 중'));
-              }
-            }
-            )
-
-    );
+            return BoxWidget('제조 Lead-time', state, 'narrow').make(
+                onTap: () {
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => lead_time()));
+                },
+                dbRelatedContentBuilder: FutureBuilder(
+                    future: conn.sendQuery(
+                        'SELECT DueDate as dueDate FROM LeadTime ORDER BY DueDate ASC;'),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return Text.rich(
+                            detailPageTheme.makeHeaderText('[${dif}]days]'));
+                      } else {
+                        return Text.rich(TextSpan(text: '불러오는 중'));
+                      }
+                    }));
+          } else {
+            return Text.rich(TextSpan(text: '불러오는 중'));
+          }
+        });
   }
 }
