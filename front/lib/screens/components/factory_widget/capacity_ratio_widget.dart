a6d86e1d0b3642edb4b508c1122da8b6ae927e69
sseunnxx <89835693+sseunnxx@users.noreply.github.com>
2021-11-18 23:40:20 +0900

    안전위험경고 연결

--- front/lib/screens/components/factory_widget/capacity_ratio_widget.dart
+++ front/lib/screens/components/factory_widget/capacity_ratio_widget.dart
@@ -13,6 +13,7 @@ class Capacity_Ratio_Widget extends StatefulWidget {
 class _Capacity_Ratio_Widget extends State<Capacity_Ratio_Widget> {
   late TooltipBehavior _toolTipBehavior;
 
+  var state = 'danger';
 
   void initState() {
     _toolTipBehavior = TooltipBehavior();
@@ -23,70 +24,92 @@ class _Capacity_Ratio_Widget extends State<Capacity_Ratio_Widget> {
   Widget build(BuildContext context) {
     bool isScrolling = false;
 
-    return BoxWidget('설비가동률', 'danger', 'narrow').make(
-        onTap: () {
-          Navigator.of(context)
-              .push(MaterialPageRoute(builder: (context) => capacity_ratio()));
-        },
-        dbRelatedContentBuilder: FutureBuilder(
-            future: conn.sendQuery(
-                'SELECT Goal, Achievement FROM CompletionRate WHERE Category=\'OPRCM\' ORDER BY RecordedDate DESC;'),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                var result = snapshot.data as List<Map<String, dynamic>>;
-                var goal = double.parse(result[0]['Goal']);
-                var achievement = double.parse(result[0]['Achievement']);
-                var achieveRate = (achievement / goal) * 100;
+    return FutureBuilder(
+        future: conn.sendQuery(
+            'SELECT Goal, Achievement FROM CompletionRate WHERE Category=\'OPRCM\' ORDER BY RecordedDate DESC;'),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            var result = snapshot.data as List<Map<String, dynamic>>;
+            var goal = double.parse(result[0]['Goal']);
+            var achievement = double.parse(result[0]['Achievement']);
+            var achieveRate = (achievement / goal) * 100;
 
-                List<ChartData> capacityData = [
-                  ChartData('complete', achieveRate),
-                  ChartData('uncomplete', 100 - achieveRate)
-                ];
+            if (achieveRate > 70)
+              state = 'safe';
+            else if (achieveRate > 50)
+              state = 'warning';
+            else
+              state = 'danger';
 
-                return SfCircularChart(
-                    tooltipBehavior: _toolTipBehavior,
-                    onChartTouchInteractionMove: (_Capacity_Ratio_Widget){
-                      isScrolling = true;
-                    },
-                    onChartTouchInteractionUp: (_Capacity_Ratio_Widget) {
-                      if(isScrolling == false) {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => capacity_ratio()));
+            return BoxWidget('설비가동률', state, 'narrow').make(
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => capacity_ratio()));
+                },
+                dbRelatedContentBuilder: FutureBuilder(
+                    future: conn.sendQuery(
+                        'SELECT Goal, Achievement FROM CompletionRate WHERE Category=\'OPRCM\' ORDER BY RecordedDate DESC;'),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        var result =
+                            snapshot.data as List<Map<String, dynamic>>;
+                        var goal = double.parse(result[0]['Goal']);
+                        var achievement =
+                            double.parse(result[0]['Achievement']);
+                        var achieveRate = (achievement / goal) * 100;
+
+                        List<ChartData> capacityData = [
+                          ChartData('complete', achieveRate),
+                          ChartData('uncomplete', 100 - achieveRate)
+                        ];
+
+                        return SfCircularChart(
+                            tooltipBehavior: _toolTipBehavior,
+                            onChartTouchInteractionMove:
+                                (_Capacity_Ratio_Widget) {
+                              isScrolling = true;
+                            },
+                            onChartTouchInteractionUp:
+                                (_Capacity_Ratio_Widget) {
+                              if (isScrolling == false) {
+                                Navigator.of(context).push(MaterialPageRoute(
+                                    builder: (context) => capacity_ratio()));
+                              }
+                              isScrolling = false;
+                            },
+                            onChartTouchInteractionDown:
+                                (_Capacity_Ratio_Widget) {
+                              isScrolling = false;
+                            },
+                            palette: <Color>[
+                              Color.fromRGBO(105, 168, 248, 0),
+                              Color.fromRGBO(253, 122, 93, 0),
+                            ],
+                            series: <CircularSeries>[
+                              DoughnutSeries<ChartData, String>(
+                                  radius: '100%',
+                                  dataSource: capacityData,
+                                  xValueMapper: (ChartData data, _) => data.x,
+                                  yValueMapper: (ChartData data, _) => data.y,
+                                  dataLabelMapper: (ChartData data, _) =>
+                                      data.x,
+                                  dataLabelSettings: DataLabelSettings(
+                                      showCumulativeValues: false,
+                                      showZeroValue: false,
+                                      isVisible: true,
+                                      textStyle: TextStyle(
+                                          color: Colors.black87,
+                                          fontSize: 30.w,
+                                          fontFamily: 'applesdneob')))
+                            ]);
+                      } else {
+                        return Text.rich(TextSpan(text: '불러오는 중'));
                       }
-                      isScrolling = false;
-                    },
-                    onChartTouchInteractionDown: (_Capacity_Ratio_Widget){
-                      isScrolling = false;
-                    },
-                    palette: <Color>[
-                      Color.fromRGBO(105, 168, 248, 0),
-                      Color.fromRGBO(253, 122, 93, 0),
-                    ],
-                    series: <CircularSeries>[
-                      DoughnutSeries<ChartData, String>(
-                          radius: '100%',
-                          dataSource: capacityData,
-                          xValueMapper: (ChartData data, _) => data.x,
-                          yValueMapper: (ChartData data, _) => data.y,
-                          dataLabelMapper: (ChartData data, _) => data.x,
-                          dataLabelSettings: DataLabelSettings(
-                              showCumulativeValues: false,
-                              showZeroValue: false,
-                              isVisible: true,
-                              textStyle: TextStyle(
-                                  color: Colors.black87,
-                                  fontSize: 30.w,
-                                  fontFamily: 'applesdneob'
-                              )
-                          )
-                      )
-                    ]);
-              } else {
-                return Text.rich(TextSpan(text: '불러오는 중'));
-              }
-            }
-            )
-    );
+                    }));
+          } else {
+            return Text.rich(TextSpan(text: '불러오는 중'));
+          }
+        });
   }
 }
 
