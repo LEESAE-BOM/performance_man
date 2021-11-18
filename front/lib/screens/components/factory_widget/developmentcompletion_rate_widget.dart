5359939ba77b0be551d5fd4eebe49a40c76025ea
sseunnxx <89835693+sseunnxx@users.noreply.github.com>
2021-11-19 00:08:54 +0900

    안전경고위험 연결

--- front/lib/screens/components/factory_widget/developmentcompletion_rate_widget.dart
+++ front/lib/screens/components/factory_widget/developmentcompletion_rate_widget.dart
@@ -13,14 +13,15 @@ class Developmentcompletion_Rate_Widget extends StatefulWidget {
       _Developmentcompletion_Rate_Widget();
 }
 
-class _Developmentcompletion_Rate_Widget extends State<Developmentcompletion_Rate_Widget> {
+class _Developmentcompletion_Rate_Widget
+    extends State<Developmentcompletion_Rate_Widget> {
   late TooltipBehavior _toolTipBehavior;
   List<ChartData> developmentData = [];
+  var state = 'safe';
 
   void initState() {
     _toolTipBehavior = TooltipBehavior();
-    for(int i=1; i<=12; i++)
-      developmentData.add(ChartData('$i월', 0));
+    for (int i = 1; i <= 12; i++) developmentData.add(ChartData('$i월', 0));
     super.initState();
   }
 
@@ -28,68 +29,96 @@ class _Developmentcompletion_Rate_Widget extends State<Developmentcompletion_Rat
   Widget build(BuildContext context) {
     bool isScrolling = false;
 
-    return BoxWidget("개발완료율", "safe", "wide").make(
-        onTap: () {
-          Navigator.of(context).push(MaterialPageRoute(
-              builder: (context) => development_completion_rate()));
-        },
-        dbRelatedContentBuilder: FutureBuilder(
-            future: conn.sendQuery(
-                'SELECT RecordedDate, Goal, Achievement FROM CompletionRate WHERE Category=\'DVLCM\' ORDER BY Achievement/Goal DESC;'),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                var result = snapshot.data as List<Map<String, dynamic>>;
-                var thisMonth = DateTime.parse(result[0]['RecordedDate']).month;
+    return FutureBuilder(
+        future: conn.sendQuery(
+            'SELECT RecordedDate, Goal, Achievement FROM CompletionRate WHERE Category=\'DVLCM\' ORDER BY Achievement/Goal DESC;'),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            var result = snapshot.data as List<Map<String, dynamic>>;
+            var goal = double.parse(result[0]['Goal']);
+            var achievement = double.parse(result[0]['Achievement']);
+            var achieveRate = (achievement / goal) * 100;
 
-                for (int i = 1; i < min(thisMonth, result.length); i++)
-                  developmentData[thisMonth - i].data =
-                      double.parse(result[i]['Achievement']) /
-                          double.parse(result[i]['Goal']) *
-                          100;
+            if (achieveRate > 80)
+              state = 'safe';
+            else if (achieveRate > 50)
+              state = 'warning';
+            else
+              state = 'danger';
 
-                return SfCartesianChart(
-                  tooltipBehavior: _toolTipBehavior,
-                  onChartTouchInteractionMove: (_Developmentcompletion_Rate_Widget){
-                    isScrolling = true;
-                  },
-                  onChartTouchInteractionUp: (_Developmentcompletion_Rate_Widget) {
-                    if(isScrolling == false) {
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => development_completion_rate()));
-                    }
-                    isScrolling = false;
-                  },
-                  onChartTouchInteractionDown: (_Developmentcompletion_Rate_Widget){
-                    isScrolling = false;
-                  },
-                  palette: <Color>[
-                    Colors.teal,
-                  ],
-                  series: <ChartSeries>[
-                    LineSeries<ChartData, String>(
-                        dataSource: developmentData,
-                        xValueMapper: (ChartData sales, _) => sales.month,
-                        yValueMapper: (ChartData sales, _) => sales.data,
-                        dataLabelSettings: DataLabelSettings(isVisible: true),
-                        markerSettings: MarkerSettings(
-                            isVisible: true,
-                            color: Colors.teal,
-                            borderColor: Colors.white)),
-                  ],
-                  primaryXAxis: CategoryAxis(
-                    majorGridLines: MajorGridLines(width: 0),
-                  ),
-                  primaryYAxis: NumericAxis(
-                    majorGridLines: MajorGridLines(width: 0),
-                  ),
-                  plotAreaBorderWidth: 0,
-                );
-              } else {
-                return Text.rich(TextSpan(text: '불러오는 중'));
-              }
-            }
-            )
-    );
+            return BoxWidget("개발완료율", state, "wide").make(
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => development_completion_rate()));
+                },
+                dbRelatedContentBuilder: FutureBuilder(
+                    future: conn.sendQuery(
+                        'SELECT RecordedDate, Goal, Achievement FROM CompletionRate WHERE Category=\'DVLCM\' ORDER BY Achievement/Goal DESC;'),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        var result =
+                            snapshot.data as List<Map<String, dynamic>>;
+                        var thisMonth =
+                            DateTime.parse(result[0]['RecordedDate']).month;
+
+                        for (int i = 1; i < min(thisMonth, result.length); i++)
+                          developmentData[thisMonth - i].data =
+                              double.parse(result[i]['Achievement']) /
+                                  double.parse(result[i]['Goal']) *
+                                  100;
+
+                        return SfCartesianChart(
+                          tooltipBehavior: _toolTipBehavior,
+                          onChartTouchInteractionMove:
+                              (_Developmentcompletion_Rate_Widget) {
+                            isScrolling = true;
+                          },
+                          onChartTouchInteractionUp:
+                              (_Developmentcompletion_Rate_Widget) {
+                            if (isScrolling == false) {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) =>
+                                      development_completion_rate()));
+                            }
+                            isScrolling = false;
+                          },
+                          onChartTouchInteractionDown:
+                              (_Developmentcompletion_Rate_Widget) {
+                            isScrolling = false;
+                          },
+                          palette: <Color>[
+                            Colors.teal,
+                          ],
+                          series: <ChartSeries>[
+                            LineSeries<ChartData, String>(
+                                dataSource: developmentData,
+                                xValueMapper: (ChartData sales, _) =>
+                                    sales.month,
+                                yValueMapper: (ChartData sales, _) =>
+                                    sales.data,
+                                dataLabelSettings:
+                                    DataLabelSettings(isVisible: true),
+                                markerSettings: MarkerSettings(
+                                    isVisible: true,
+                                    color: Colors.teal,
+                                    borderColor: Colors.white)),
+                          ],
+                          primaryXAxis: CategoryAxis(
+                            majorGridLines: MajorGridLines(width: 0),
+                          ),
+                          primaryYAxis: NumericAxis(
+                            majorGridLines: MajorGridLines(width: 0),
+                          ),
+                          plotAreaBorderWidth: 0,
+                        );
+                      } else {
+                        return Text.rich(TextSpan(text: '불러오는 중'));
+                      }
+                    }));
+          } else {
+            return Text.rich(TextSpan(text: '불러오는 중'));
+          }
+        });
   }
 }
 
