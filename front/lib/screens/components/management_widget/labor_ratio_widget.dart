862cbea9dd5f7c65a3fb940ae5076f5ac8b171e8
송다인 <70890888+ssongdda@users.noreply.github.com>
2021-11-21 16:09:50 +0900

    쿼리 중복 수정

--- front/lib/screens/components/management_widget/labor_ratio_widget.dart
+++ front/lib/screens/components/management_widget/labor_ratio_widget.dart
@@ -5,6 +5,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_app/mysql_connect.dart';
 import 'package:flutter_app/box_widget.dart';
 import 'package:flutter_app/screens/management/management_screen.dart';
+
 //작은 위젯
 class Labor_Ratio_Widget extends StatefulWidget {
   @override
@@ -19,7 +20,8 @@ class _Labor_Ratio_Widget extends State<Labor_Ratio_Widget> {
     List<ChartData> laborData = [];
 
     return FutureBuilder(
-        future: conn.sendQuery('SELECT RecordedDate, Category, Money * 1000 as Money FROM Money WHERE Category like \'%LBR\' ORDER BY RecordedDate DESC;'),
+        future: conn.sendQuery(
+            'SELECT RecordedDate, Category, Money * 1000 as Money FROM Money WHERE Category like \'%LBR\' ORDER BY RecordedDate DESC;'),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
             var result = snapshot.data as List<Map<String, dynamic>>;
@@ -28,93 +30,73 @@ class _Labor_Ratio_Widget extends State<Labor_Ratio_Widget> {
             double IDLBRRate = 0;
             double DCLBRRate = 0;
 
-            int thisYear = DateTime
-                .parse(result[0]['RecordedDate'])
-                .year;
-            int thisMonth = DateTime
-                .parse(result[0]['RecordedDate'])
-                .month;
+            int thisYear = DateTime.parse(result[0]['RecordedDate']).year;
+            int thisMonth = DateTime.parse(result[0]['RecordedDate']).month;
 
             for (int i = 0; i < result.length; i++) {
-              int year = DateTime
-                  .parse(result[i]['RecordedDate'])
-                  .year;
-              int month = DateTime
-                  .parse(result[i]['RecordedDate'])
-                  .month;
-              if (thisYear == year && thisMonth == month)
-                if (result[i]['Category'] == 'DCLBR')
-                  totalDCLBR += double.parse(result[i]['Money']);
-                else if (result[i]['Category'] == 'IDLBR')
-                  totalIDLBR += double.parse(result[i]['Money']);
+              int year = DateTime.parse(result[i]['RecordedDate']).year;
+              int month = DateTime.parse(result[i]['RecordedDate']).month;
+              if (thisYear == year &&
+                  thisMonth == month) if (result[i]['Category'] == 'DCLBR')
+                totalDCLBR += double.parse(result[i]['Money']);
+              else if (result[i]['Category'] == 'IDLBR')
+                totalIDLBR += double.parse(result[i]['Money']);
             }
 
             laborData.clear();
             IDLBRRate = (totalIDLBR / (totalIDLBR + totalDCLBR)) * 100;
             DCLBRRate = (totalDCLBR / (totalIDLBR + totalDCLBR)) * 100;
 
-            if( IDLBRRate>= DCLBRRate)
-              state[3]='safe';
-            else if( IDLBRRate>= DCLBRRate-10)
-              state[3]='warning';
+            laborData = [
+              ChartData('직접인건비', DCLBRRate, '${DCLBRRate.round()}%'),
+              ChartData('간접인건비', IDLBRRate, '${IDLBRRate.round()}%')
+            ];
+
+            if (IDLBRRate >= DCLBRRate)
+              state[3] = 'safe';
+            else if (IDLBRRate >= DCLBRRate - 10)
+              state[3] = 'warning';
             else
-              state[3]='danger';
+              state[3] = 'danger';
             return BoxWidget('인건비율', state[3], 'narrow').make(
                 onTap: () {
-                  Navigator.of(context)
-                      .push(MaterialPageRoute(builder: (context) => labor_ratio()));
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => labor_ratio()));
                 },
-                dbRelatedContentBuilder: FutureBuilder(
-                    future: conn.sendQuery(
-                        'SELECT RecordedDate, Category, Money * 1000 as Money FROM Money WHERE Category like \'%LBR\' ORDER BY RecordedDate DESC;'),
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData) {
-                        laborData = [
-                          ChartData('직접인건비', DCLBRRate, '${DCLBRRate.round()}%'),
-                          ChartData('간접인건비', IDLBRRate, '${IDLBRRate.round()}%')
-                        ];
-                        return SfCircularChart(
-                            onChartTouchInteractionMove: (_Labor_Ratio_Widget) {
-                              isScrolling = true;
-                            },
-                            onChartTouchInteractionUp: (_Labor_Ratio_Widget) {
-                              if (isScrolling == false) {
-                                Navigator.of(context).push(MaterialPageRoute(
-                                    builder: (context) => labor_ratio()));
-                              }
-                              isScrolling = false;
-                            },
-                            onChartTouchInteractionDown: (_Labor_Ratio_Widget) {
-                              isScrolling = false;
-                            },
-                            palette: <Color>[
-                              Colors.indigo,
-                              Colors.lightBlueAccent,
-                            ],
-                            series: <CircularSeries>[
-                              PieSeries<ChartData, String>(
-                                  radius: '100%',
-                                  dataSource: laborData,
-                                  xValueMapper: (ChartData data, _) => data.x,
-                                  yValueMapper: (ChartData data, _) => data.y,
-                                  dataLabelMapper: (ChartData data, _) => data.text,
-                                  dataLabelSettings: DataLabelSettings(
-                                      isVisible: true,
-                                      textStyle: TextStyle(fontSize: 50.w,
-                                          fontFamily: 'applesdneob'))),
-                            ]
-                        );
-                      } else {
-                        return Text.rich(TextSpan(text: '불러오는 중'));
+                dbRelatedContentBuilder: SfCircularChart(
+                    onChartTouchInteractionMove: (_Labor_Ratio_Widget) {
+                      isScrolling = true;
+                    },
+                    onChartTouchInteractionUp: (_Labor_Ratio_Widget) {
+                      if (isScrolling == false) {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => labor_ratio()));
                       }
-                    }
-                )
-            );}
-          else {
+                      isScrolling = false;
+                    },
+                    onChartTouchInteractionDown: (_Labor_Ratio_Widget) {
+                      isScrolling = false;
+                    },
+                    palette: <Color>[
+                      Colors.indigo,
+                      Colors.lightBlueAccent,
+                    ],
+                    series: <CircularSeries>[
+                      PieSeries<ChartData, String>(
+                          radius: '100%',
+                          dataSource: laborData,
+                          xValueMapper: (ChartData data, _) => data.x,
+                          yValueMapper: (ChartData data, _) => data.y,
+                          dataLabelMapper: (ChartData data, _) => data.text,
+                          dataLabelSettings: DataLabelSettings(
+                              isVisible: true,
+                              textStyle: TextStyle(
+                                  fontSize: 50.w, fontFamily: 'applesdneob'))),
+                    ]));
+          } else {
             return Text.rich(TextSpan(text: '불러오는 중'));
           }
-        }
-    );
+        });
   }
 }
 
--- front/lib/screens/components/management_widget/sales_widget.dart
+++ front/lib/screens/components/management_widget/sales_widget.dart
@@ -4,49 +4,39 @@ import 'package:flutter_app/mysql_connect.dart';
 import 'package:flutter_app/theme.dart';
 import 'package:flutter_app/box_widget.dart';
 import 'package:flutter_app/screens/management/management_screen.dart';
+
 class Sales_Widget extends StatefulWidget {
   @override
   _Sales_Widget createState() => _Sales_Widget();
 }
 
 class _Sales_Widget extends State<Sales_Widget> {
-
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-          future: conn.sendQuery('SELECT Money * 1000 as Money FROM Money WHERE Category = \'SALES\' ORDER BY RecordedDate DESC;'),
-           builder:  (context, snapshot) {
-            if (snapshot.hasData){
-              var result = snapshot.data as List<Map<String, dynamic>>;
-              int money = double.parse(result[0]['Money']).round();
-              if(money>19000000000)
-                state[5]='safe';
-              else if(money>17000000000){
-                state[5]='warning';
-              }
-              else {
-                state[5]='danger';
-              }
-              return BoxWidget('매출금액', state[5] , 'wide').make(
-                  onTap: () {Navigator.of(context).push(MaterialPageRoute(builder: (context) => sales()));},
-                  dbRelatedContentBuilder: FutureBuilder(
-                      future: conn.sendQuery('SELECT Money * 1000 as Money FROM Money WHERE Category = \'SALES\' ORDER BY RecordedDate DESC;'),
-                      builder: (context, snapshot){
-                        if(snapshot.hasData){
-                          return Text.rich(detailPageTheme.makeHeaderText('이번달 매출금액은\n[${detailPageTheme.money.format(money)}]원입니다.'));
-                        }else{
-                          return Text.rich(TextSpan(text: '불러오는 중'));
-                        }
-                      }
-                  )
-              );
+        future: conn.sendQuery(
+            'SELECT Money * 1000 as Money FROM Money WHERE Category = \'SALES\' ORDER BY RecordedDate DESC;'),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            var result = snapshot.data as List<Map<String, dynamic>>;
+            int money = double.parse(result[0]['Money']).round();
+            if (money > 19000000000)
+              state[5] = 'safe';
+            else if (money > 17000000000) {
+              state[5] = 'warning';
+            } else {
+              state[5] = 'danger';
             }
-            else{
-              return Text.rich(TextSpan(text: '불러오는 중'));
-            }
+            return BoxWidget('매출금액', state[5], 'wide').make(
+                onTap: () {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) => sales()));
+                },
+                dbRelatedContentBuilder: Text.rich(detailPageTheme.makeHeaderText(
+                    '이번달 매출금액은\n[${detailPageTheme.money.format(money)}]원입니다.')));
+          } else {
+            return Text.rich(TextSpan(text: '불러오는 중'));
           }
-    );
+        });
   }
-
-
 }
