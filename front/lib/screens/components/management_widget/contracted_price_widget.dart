1e78a4e25da7ef98c5cd4dc3fc33cfd1e05f99b8
송다인 <70890888+ssongdda@users.noreply.github.com>
2021-11-21 16:09:14 +0900

    쿼리 중복 수정

--- front/lib/screens/components/management_widget/contracted_price_widget.dart
+++ front/lib/screens/components/management_widget/contracted_price_widget.dart
@@ -4,6 +4,7 @@ import 'package:flutter_app/mysql_connect.dart';
 import 'package:flutter_app/theme.dart';
 import 'package:flutter_app/box_widget.dart';
 import 'package:flutter_app/screens/management/management_screen.dart';
+
 //큰 위젯
 class Contracted_Price_Widget extends StatefulWidget {
   @override
@@ -14,44 +15,30 @@ class _Contracted_Price_Widget extends State<Contracted_Price_Widget> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: conn.sendQuery('SELECT Price * 1000 as Money FROM Contract ORDER BY ContractDate DESC;'),
-        builder: (context, snapshot){
-          if(snapshot.hasData) {
+        future: conn.sendQuery(
+            'SELECT Price * 1000 as Money FROM Contract ORDER BY ContractDate DESC;'),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
             var result = snapshot.data as List<Map<String, dynamic>>;
             int money = double.parse(result[0]['Money']).round();
 
-            if(money>14000000000)
-              state[1]='safe';
-            else if(money>11000000000){
-              state[1]='warning';
-            }
-            else {
-              state[1]='danger';
+            if (money > 14000000000)
+              state[1] = 'safe';
+            else if (money > 11000000000) {
+              state[1] = 'warning';
+            } else {
+              state[1] = 'danger';
             }
             return BoxWidget('계약금액', state[1], 'wide').make(
                 onTap: () {
-                  Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => contracted_price()));
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => contracted_price()));
                 },
-                dbRelatedContentBuilder: FutureBuilder(
-                    future: conn.sendQuery(
-                        'SELECT Price * 1000 as Money FROM Contract ORDER BY ContractDate DESC;'),
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData) {
-                        return Text.rich(detailPageTheme.makeHeaderText(
-                            '이번달 [계약금액]은\n[${detailPageTheme.money.format(
-                                money)}]원입니다.'));
-                      } else {
-                        return Text.rich(TextSpan(text: '불러오는 중'));
-                      }
-                    }
-                )
-            );
-          }
-          else {
+                dbRelatedContentBuilder: Text.rich(detailPageTheme.makeHeaderText(
+                    '이번달 [계약금액]은\n[${detailPageTheme.money.format(money)}]원입니다.')));
+          } else {
             return Text.rich(TextSpan(text: '불러오는 중'));
           }
-        }
-    );
+        });
   }
 }
