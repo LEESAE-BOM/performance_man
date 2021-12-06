0759380cd5e017b066cf06ee461b4b6f66cba96f
이새봄 <70834586+LEESAE-BOM@users.noreply.github.com>
2021-12-06 19:43:23 +0900

    error 수정

--- front/lib/screens/components/management_widget/cash_reserve_widget.dart
+++ front/lib/screens/components/management_widget/cash_reserve_widget.dart
@@ -7,7 +7,6 @@ import 'package:intl/intl.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_app/mysql_connect.dart';
 import 'package:flutter_app/box_widget.dart';
-import 'package:flutter_app/theme.dart';
 import 'package:flutter_app/screens/management/management_screen.dart';
 
 //작은 위젯
@@ -17,6 +16,8 @@ class Cash_Reserve_Widget extends StatefulWidget {
 }
 
 class _Cash_Reserve_Widget extends State<Cash_Reserve_Widget> {
+  List<ChartData> cashData = [];
+
   @override
   Widget build(BuildContext context) {
     bool isScrolling = false;
@@ -25,72 +26,62 @@ class _Cash_Reserve_Widget extends State<Cash_Reserve_Widget> {
             'SELECT YEAR(RecordedDate) as Year, SUM(Money) * 1000 as Money FROM Money WHERE Category=\'MONEY\' GROUP BY Year ORDER BY Year;'),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            List<ChartData> cashData = [];
             var result = snapshot.data as List<Map<String, dynamic>>;
-            if (result.length > 0) {
-              for (int i = 0; i < min(result.length, 3); i++) {
-                cashData.add(ChartData((int.parse(result[i]['Year'])),
-                    double.parse(result[i]['Money'])));
-              }
-              if (double.parse(result[0]['Year']) > 200000000000)
-                state[0] = 'safe';
-              else if (double.parse(result[0]['Year']) > 170000000000) {
-                state[0] = 'warning';
-              } else {
-                state[0] = 'danger';
-              }
-              return BoxWidget('현금보유액', state[0], 'wide').make(
-                  onTap: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => cash_reserve()));
-                  },
-                  dbRelatedContentBuilder: SfCartesianChart(
-                      onChartTouchInteractionMove: (_Cash_Reserve_Widget) {
-                        isScrolling = true;
-                      },
-                      onChartTouchInteractionUp: (_Cash_Reserve_Widget) {
-                        if (isScrolling == false) {
-                          Navigator.of(context).push(MaterialPageRoute(
-                              builder: (context) => cash_reserve()));
-                        }
-                        isScrolling = false;
-                      },
-                      onChartTouchInteractionDown: (_Cash_Reserve_Widget) {
-                        isScrolling = false;
-                      },
-                      primaryXAxis: CategoryAxis(
-                        majorGridLines: MajorGridLines(width: 0),
-                        labelStyle: TextStyle(
-                            fontFamily: 'applesdneom', fontSize: 35.w),
-                      ),
-                      primaryYAxis: NumericAxis(
-                        majorGridLines: MajorGridLines(width: 0),
-                        edgeLabelPlacement: EdgeLabelPlacement.shift,
-                        numberFormat: NumberFormat.compact(locale: "ko_KR"),
-                        labelStyle: TextStyle(
-                            fontFamily: 'applesdneom', fontSize: 35.w),
-                      ),
-                      plotAreaBorderWidth: 0,
-                      palette: <Color>[
-                        Colors.teal,
-                      ],
-                      series: <ChartSeries>[
-                        BarSeries<ChartData, int>(
-                            dataSource: cashData,
-                            xValueMapper: (ChartData cash, _) => cash.x,
-                            yValueMapper: (ChartData cash, _) => cash.y,
-                            dataLabelSettings:
-                                DataLabelSettings(isVisible: true),
-                            width: 0.6,
-                            spacing: 0.2),
-                      ]));
-            } else {
-              state[0] = 'none';
-              return BoxWidget('현금보유액', state[0], 'wide').make(
-                onTap: (){},
-                dbRelatedContentBuilder: Text.rich(detailPageTheme.makeHeaderText(
-                    '데이터가 없습니다.')));
+            for (int i = 0; i <= min(result.length, 2); i++) {
+              cashData.add(ChartData((double.parse(result[i]['Year'])).round(),
+                  double.parse(result[i]['Money'])));
             }
+            if (double.parse(result[2]['Year']) > 200000000000)
+              state[0] = 'safe';
+            else if (double.parse(result[2]['Year']) > 170000000000) {
+              state[0] = 'warning';
+            } else {
+              state[0] = 'danger';
+            }
+            return BoxWidget('현금보유액', state[0], 'wide').make(
+                onTap: () {
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => cash_reserve()));
+                },
+                dbRelatedContentBuilder: SfCartesianChart(
+                    onChartTouchInteractionMove: (_Cash_Reserve_Widget) {
+                      isScrolling = true;
+                    },
+                    onChartTouchInteractionUp: (_Cash_Reserve_Widget) {
+                      if (isScrolling == false) {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => cash_reserve()));
+                      }
+                      isScrolling = false;
+                    },
+                    onChartTouchInteractionDown: (_Cash_Reserve_Widget) {
+                      isScrolling = false;
+                    },
+                    primaryXAxis: CategoryAxis(
+                      majorGridLines: MajorGridLines(width: 0),
+                      labelStyle:
+                          TextStyle(fontFamily: 'applesdneom', fontSize: 35.w),
+                    ),
+                    primaryYAxis: NumericAxis(
+                      majorGridLines: MajorGridLines(width: 0),
+                      edgeLabelPlacement: EdgeLabelPlacement.shift,
+                      numberFormat: NumberFormat.compact(locale: "ko_KR"),
+                      labelStyle:
+                          TextStyle(fontFamily: 'applesdneom', fontSize: 35.w),
+                    ),
+                    plotAreaBorderWidth: 0,
+                    palette: <Color>[
+                      Colors.teal,
+                    ],
+                    series: <ChartSeries>[
+                      BarSeries<ChartData, int>(
+                          dataSource: cashData,
+                          xValueMapper: (ChartData cash, _) => cash.x,
+                          yValueMapper: (ChartData cash, _) => cash.y,
+                          dataLabelSettings: DataLabelSettings(isVisible: true),
+                          width: 0.6,
+                          spacing: 0.2),
+                    ]));
           } else {
             return Text('불러오는 중');
           }
